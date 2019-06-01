717980294c723385d63034c56218a55c04510f8d
Michael Beaton <beatonma@gmail.com>
2019-06-02 00:37:00 +0100

    Default settings for runtests.py. Apps may add/override with file `appname.tests.config.test_settings`

---
+++ defaulttestconfig/test_settings_default.py
@@ -0,0 +1,32 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-spec',
+    '--spec-color',
+    '--logging-clear-handlers',
+]
+
+ALLOWED_HOSTS = ['localhost']
+
+
+SECRET_KEY = 'some-test-key'
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+]
+
+MIDDLEWARE = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+LANGUAGE_CODE = 'en-gb'

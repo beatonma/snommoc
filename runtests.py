2780973ecf081159048d4ec28f0de14508c645b2
Michael Beaton <beatonma@gmail.com>
2019-06-02 00:35:13 +0100

    Generalised to run tests for all modules listed in TEST_APPS

--- runtests.py
+++ runtests.py
@@ -1,15 +1,73 @@
+"""
+To enable a module for testing:
+- Create a Django settings file called test_settings.py in {appname}/tests//config/
+- Add the module name to TEST_APPS in this file.
+"""
 import os
 import sys
+from importlib import import_module
+from importlib.util import find_spec
 
 import django
+from django.apps import apps
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == '__main__':
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'api.tests.config.test_settings'
-    django.setup()
+from defaulttestconfig.test_settings_default import *
 
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner()
-    failures = test_runner.run_tests(['api.tests'])
-    sys.exit(bool(failures))
+TEST_APPS = [
+    'api',
+    'serviceglue',
+]
+
+
+def reset_settings():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'defaulttestconfig.test_settings_default')
+    settings.ALLOWED_HOSTS = ALLOWED_HOSTS
+    settings.INSTALLED_APPS = INSTALLED_APPS
+    settings.MIDDLEWARE = MIDDLEWARE
+    settings.DATABASES = DATABASES
+    settings.SECRET_KEY = SECRET_KEY
+    settings.LANGUAGE_CODE = LANGUAGE_CODE
+    settings.TEST_RUNNER = TEST_RUNNER
+    settings.NOSE_ARGS = NOSE_ARGS
+
+
+def run_app_tests(app_name):
+    settings_path = f'{app_name}.tests.config.test_settings'
+    try:
+        if find_spec(settings_path):
+            print(f'Using app settings file {settings_path}')
+            os.environ['DJANGO_SETTINGS_MODULE'] = settings_path
+    except ModuleNotFoundError:
+        print(f'App \'{app_name}\' is using the default test settings')
+
+    reset_settings()
+    settings.INSTALLED_APPS.append(app_name)
+    django.setup()
+    apps.set_installed_apps(settings.INSTALLED_APPS)
+
+    test_runner = get_runner(settings)()
+    test_results = test_runner.run_tests([f'{app_name}.tests'])
+    return test_results
+
+
+def _print_results(test_results):
+    print('Results:')
+    for app, result in test_results.items():
+        print(f'  {app}  {"OK" if result == 0 else result}')
+
+
+if __name__ == '__main__':
+    all_passed = True
+    results = {}
+    for module in TEST_APPS:
+        failures = run_app_tests(module)
+        results[module] = failures
+        all_passed = all_passed and bool(failures)
+
+    if all_passed:
+        sys.exit(1)
+
+    _print_results(results)
+    sys.exit(0)

44cec7fbdc03dc2fd163c898f24c79c1200274c2
Michael Beaton <beatonma@gmail.com>
2019-06-02 11:15:47 +0100

    Moved standard settings to defaulttestconfig.test_settings_default to minimise per-app repetition.

--- api/tests/config/test_settings.py
+++ api/tests/config/test_settings.py
@@ -1,33 +1,7 @@
-import os
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-    '--with-spec',
-    '--spec-color',
-    '--logging-clear-handlers',
-]
-
-ALLOWED_HOSTS = [
-    'localhost',
-]
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-SECRET_KEY = 'some-test-key'
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'api',
-    'api.tests',
-]
-
-MIDDLEWARE = []
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
-    }
-}
+from defaulttestconfig.test_settings_default import *
 
 ROOT_URLCONF = 'api.tests.config.test_urls'
+
+INSTALLED_APPS += [
+    'django.contrib.auth',
+]

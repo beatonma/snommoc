f6add73de7c82d5dda308533733fceda92b0eb43
Michael Beaton <beatonma@gmail.com>
2019-06-08 22:13:07 +0100

    Common values for tests

---
+++ repository/tests/constants.py
@@ -0,0 +1,21 @@
+EXAMPLE_NAME = 'Frank Spencer'
+EXAMPLE_ALIAS = 'Franky Spencer'
+EXAMPLE_PUK_ID = 17
+EXAMPLE_TWFY_ID = 10040
+
+PARTY_NAME = 'Normal Party'
+PARTY_NAME_LONG = 'The Long Normal Party'
+PARTY_NAME_SHORT = 'NP'
+
+CONSTITUENCY = 'Hellas Planitia'
+
+EMAIL = 'frank.spencer@fakeparliament.uk'
+
+PHONE_CONSTITUENCY = '03069990924'
+PHONE_PARLIAMENT = '02079460513'
+
+WIKIPEDIA = 'Frank_Spencer_(Fake_Politician)'
+WEBSITE = 'https://fakefrankspencermp.co.uk'
+
+INTEREST_POLITICAL = 'Giraffes'
+INTEREST_COUNTRY = 'Utopia Planitia'

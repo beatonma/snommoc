625fbd1337d02ccd4dd8e9b81fc175f3c6c8ef34
Michael Beaton <beatonma@gmail.com>
2019-06-09 20:20:37 +0100

    Replaced Mp import with 'Mp' string for import consistency

--- repository/models/constituency.py
+++ repository/models/constituency.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-from repository.models import Mp
-
 
 class Constituency(models.Model):
     name = models.CharField(max_length=64)
-    mp = models.OneToOneField(Mp, on_delete=models.CASCADE)
+    mp = models.OneToOneField('Mp', on_delete=models.CASCADE, null=True)

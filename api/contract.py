f8f4f7260c70fd24eecfd82a20929182ba2c363b
Michael Beaton <beatonma@gmail.com>
2019-06-09 18:01:16 +0100

    Standardised object names for API responses

---
+++ api/contract.py
@@ -0,0 +1,19 @@
+"""Standard key names for JSON responses returned by this API.
+"""
+
+NAME = 'name'
+ALIASES = 'aliases'
+PARTY = 'party'
+CONSTITUENCY = 'constituency'
+INTERESTS = 'interests'
+INTERESTS_POLITICAL = 'interests_political'
+INTERESTS_COUNTRIES = 'interests_countries'
+THEYWORKFORYOU_ID = 'twfy_id',
+PARLIAMENTDOTUK_ID = 'puk_id',
+
+PERSONAL_LINKS = 'links'
+EMAIL = 'email'
+WEBLINKS = 'weblinks'
+WIKIPEDIA = 'wikipedia',
+PHONE_CONSTITUENCY = 'phone_constituency'
+PHONE_PARLIAMENTARY = 'phone_parliamentary'

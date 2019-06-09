e94d8e6b399b217be563c700ef92e3a56025c297
Michael Beaton <beatonma@gmail.com>
2019-06-09 17:45:51 +0100

    Added test for when all link data is empty

--- repository/tests/test_contact_details.py
+++ repository/tests/test_contact_details.py
@@ -6,9 +6,7 @@ from repository.tests import constants
 
 class ContactDetailsTest(LocalTestCase):
     def setUp(self) -> None:
-        self.mp = Mp.create(
-            name=constants.EXAMPLE_NAME,
-            save=True)
+        self.mp = Mp.create(name=constants.EXAMPLE_NAME)
 
     def test_linksdotcreate(self):
         links = PersonalLinks.create(
@@ -16,15 +14,16 @@ class ContactDetailsTest(LocalTestCase):
             email=constants.EMAIL,
             phone_parliament=constants.PHONE_PARLIAMENT,
             phone_constituency=constants.PHONE_CONSTITUENCY,
-            weblinks=[
-                constants.WEBSITE,
-            ],
-            wikipedia=constants.WIKIPEDIA
-        )
+            weblinks=constants.WEBLINKS,
+            wikipedia=constants.WIKIPEDIA)
 
         self.assertEqual(len(links.weblinks.all()), 1)
-        self.assertEqual(links.weblinks.first().url, constants.WEBSITE)
+        self.assertEqual(links.weblinks.first().url, constants.WEBLINKS[0])
         self.assertEqual(links.email, constants.EMAIL)
         self.assertEqual(links.wikipedia, constants.WIKIPEDIA)
         self.assertEqual(links.phone_constituency, '+443069990924')
         self.assertEqual(links.phone_parliament, '+442079460513')
+
+    def test_links_dot_create_with_no_data_should_be_none(self):
+        links = PersonalLinks.create(person=self.mp)
+        self.assertIsNone(links)

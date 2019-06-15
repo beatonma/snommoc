27cd638048ed850004a4f14ff8510becbec6941a
Michael Beaton <beatonma@gmail.com>
2019-06-15 18:40:07 +0100

    Added BaseTestCase with assert_nospace_equal - useful for asserting value equality of (for example) phone numbers regardless of space formatting.

--- basetest/testcase.py
+++ basetest/testcase.py
@@ -5,8 +5,17 @@ from django.test import TestCase
 from basetest.args import RUNTESTS_CLARGS
 
 
-class LocalTestCase(TestCase):
-    """Tests that use only local data - no network calls!
+class BaseTestCase(TestCase):
+    def assert_nospace_equal(self, first, second, msg=''):
+        """Assert that the two values are the same, ignoring spaces."""
+        self.assertEqual(
+            first.replace(r' ', ''),
+            second.replace(r' ', ''),
+            msg=msg)
+
+
+class LocalTestCase(BaseTestCase):
+    """Tests that use only local data - no external network calls!
 
     Always run when runtests.py is run.
     """
@@ -14,7 +23,7 @@ class LocalTestCase(TestCase):
 
 
 @skipIf(not RUNTESTS_CLARGS.network, reason='Network calls disabled by default')
-class NetworkTestCase(TestCase):
+class NetworkTestCase(BaseTestCase):
     """Tests that interact a remote server.
 
     Only run when specifically enabled via command line `-network` flag::

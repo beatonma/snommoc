bc550df859ae078db6efc6e4eeaaf743a6c677f8
Michael Beaton <beatonma@gmail.com>
2019-06-16 16:50:24 +0100

    Renamed constants.py -> values.py
    Replaced mp_to_json per-value assertions to a full-object equality
    assertion with values.MP_STRUCTURE

--- repository/tests/test_mp_properties.py
+++ repository/tests/test_mp_properties.py
@@ -1,13 +1,10 @@
-from api import contract as api_contract
 from basetest.testcase import LocalTestCase
 from repository.models import (
     Mp,
     Party,
     Constituency,
-    NameAlias,
 )
-from repository.tests import constants
-from repository.tests.constants import EXAMPLE_ALIASES
+from repository.tests import values
 
 
 class MpPropertiesTest(LocalTestCase):
@@ -15,85 +12,61 @@ class MpPropertiesTest(LocalTestCase):
 
     def setUp(self) -> None:
         self.mp = Mp.create(
-            name=constants.EXAMPLE_NAME,
-            puk=constants.EXAMPLE_PUK_ID,
-            twfy=constants.EXAMPLE_TWFY_ID)
+            name=values.EXAMPLE_NAME,
+            puk=values.EXAMPLE_PUK_ID,
+            twfy=values.EXAMPLE_TWFY_ID)
 
     def test_set_party(self):
         self.mp.party = Party.objects.create(
-            name=constants.PARTY_NAME,
-            long_name=constants.PARTY_NAME_LONG,
-            short_name=constants.PARTY_NAME_SHORT)
-        self.assertEqual(self.mp.party.name, constants.PARTY_NAME)
+            name=values.PARTY_NAME,
+            long_name=values.PARTY_NAME_LONG,
+            short_name=values.PARTY_NAME_SHORT)
+        self.assertEqual(self.mp.party.name, values.PARTY_NAME)
 
     def test_set_constituency(self):
         constituency = Constituency.objects.create(
-            name=constants.CONSTITUENCY,
+            name=values.CONSTITUENCY,
             mp=self.mp)
         constituency.save()
 
         self.assertEqual(
             self.mp.constituency.name,
-            constants.CONSTITUENCY)
+            values.CONSTITUENCY)
 
 
 class CompleteMpPropertiesTest(LocalTestCase):
     def setUp(self) -> None:
         Party.objects.create(
-            name=constants.PARTY_NAME,
-            short_name=constants.PARTY_NAME_SHORT,
-            long_name=constants.PARTY_NAME_LONG).save()
+            name=values.PARTY_NAME,
+            short_name=values.PARTY_NAME_SHORT,
+            long_name=values.PARTY_NAME_LONG).save()
 
-        Constituency.objects.create(name=constants.CONSTITUENCY).save()
+        Constituency.objects.create(name=values.CONSTITUENCY).save()
 
         self.mp = Mp.create(
-            name=constants.EXAMPLE_NAME,
-            puk=constants.EXAMPLE_PUK_ID,
-            twfy=constants.EXAMPLE_TWFY_ID,
-            party=constants.PARTY_NAME,
-            constituency=constants.CONSTITUENCY,
-            phone_constituency=constants.PHONE_CONSTITUENCY,
-            phone_parliamentary=constants.PHONE_PARLIAMENT,
-            email=constants.EMAIL,
-            interests_countries=constants.INTEREST_COUNTRY,
-            interests_political=constants.INTEREST_POLITICAL,
-            weblinks=constants.WEBLINKS,
-            wikipedia_path=constants.WIKIPEDIA
+            name=values.EXAMPLE_NAME,
+            aliases=values.EXAMPLE_ALIASES,
+            puk=values.EXAMPLE_PUK_ID,
+            twfy=values.EXAMPLE_TWFY_ID,
+            party=values.PARTY_NAME,
+            constituency=values.CONSTITUENCY,
+            phone_constituency=values.PHONE_CONSTITUENCY,
+            phone_parliamentary=values.PHONE_PARLIAMENT,
+            email=values.EMAIL,
+            interests_countries=values.INTEREST_COUNTRY,
+            interests_political=values.INTEREST_POLITICAL,
+            weblinks=values.WEBLINKS,
+            wikipedia_path=values.WIKIPEDIA
         )
-
-        for alias in EXAMPLE_ALIASES:
-            NameAlias.objects.create(person=self.mp, name=alias).save()
 
     def test_mp_interests_properties(self):
         self.assertListEqual(
             self.mp.political_interests,
-            constants.INTEREST_POLITICAL)
+            values.INTEREST_POLITICAL)
         self.assertListEqual(
             self.mp.countries_of_interest,
-            constants.INTEREST_COUNTRY)
+            values.INTEREST_COUNTRY)
 
     def test_mp_to_json(self):
-        json = self.mp.to_json()
-        self.assertEqual(json[api_contract.NAME], constants.EXAMPLE_NAME)
-        self.assertEqual(json[api_contract.ALIASES], constants.EXAMPLE_ALIASES)
-        self.assertEqual(json[api_contract.THEYWORKFORYOU_ID], constants.EXAMPLE_TWFY_ID)
-        self.assertEqual(json[api_contract.PARLIAMENTDOTUK_ID], constants.EXAMPLE_PUK_ID)
-        self.assertEqual(json[api_contract.PARTY], constants.PARTY_NAME)
-        self.assertEqual(json[api_contract.CONSTITUENCY], constants.CONSTITUENCY)
-
-        interests = json[api_contract.INTERESTS]
-        self.assertListEqual(interests[api_contract.INTERESTS_POLITICAL],
-                             constants.INTEREST_POLITICAL)
-        self.assertListEqual(interests[api_contract.INTERESTS_COUNTRIES],
-                             constants.INTEREST_COUNTRY)
-
-        links = json[api_contract.PERSONAL_LINKS]
-        self.assertEqual(links[api_contract.EMAIL], constants.EMAIL)
-        self.assertListEqual(links[api_contract.WEBLINKS], constants.WEBLINKS)
-        self.assertEqual(links[api_contract.WIKIPEDIA], constants.WIKIPEDIA)
-
-        phone = links[api_contract.PHONE]
-        self.assertEqual(phone[api_contract.PHONE_PARLIAMENT].replace(' ', ''),
-                         constants.PHONE_PARLIAMENT)
-        self.assertEqual(phone[api_contract.PHONE_CONSTITUENCY].replace(' ', ''),
-                         constants.PHONE_CONSTITUENCY)
+        json_data = self.mp.to_json()
+        self.assertEqual(json_data, values.MP_STRUCTURE)

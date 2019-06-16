3daecb0346b0063b39ad574779c2e0cc80bc00e1
Michael Beaton <beatonma@gmail.com>
2019-06-16 20:48:46 +0100

    Added /get_all_mps, /get_parties

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
-from api.views.people import GetMpView
+from api.views.parties import (
+    VIEW_GET_PARTIES,
+    GetPartiesView,
+)
+from api.views.people import (
+    GetMpView,
+    GetAllMPsView,
+    VIEW_GET_ALL_MPS,
+    VIEW_GET_MP,
+)
 
 urlpatterns = [
-    path('get_mp', GetMpView.as_view(), name='get_mp_view'),
+    path('get_mp', GetMpView.as_view(), name=VIEW_GET_MP),
+    path('get_all_mps', GetAllMPsView.as_view(), name=VIEW_GET_ALL_MPS),
+    path('get_parties', GetPartiesView.as_view(), name=VIEW_GET_PARTIES),
 ]

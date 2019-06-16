81dff429d24c52d95fe2fecd718fb09ac216d243
Michael Beaton <beatonma@gmail.com>
2019-06-16 16:40:25 +0100

    testing urlpatterns now appended to urlpatterns from api.urls

--- api/tests/config/test_urls.py
+++ api/tests/config/test_urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from api.tests.views import ExampleView
 
-urlpatterns = [
+from api.urls import urlpatterns
+
+urlpatterns += [
     path('decorator', ExampleView.as_view(), name='decorator_test_view'),
 ]

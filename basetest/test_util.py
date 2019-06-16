012e59a7b2b821c8413de1906a3c1f901a0ca7de
Michael Beaton <beatonma@gmail.com>
2019-06-16 16:38:11 +0100

    Added create_test_user() util function for creating, saving and returning a basic User for testing.

--- basetest/test_util.py
+++ basetest/test_util.py
@@ -1,3 +1,11 @@
+import uuid
+
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from api import contract
+
+
 def inject_context_manager(cls):
     """
     Add simple __enter__, __exit__ methods to the given class so that we
@@ -8,3 +16,16 @@ def inject_context_manager(cls):
     """
     cls.__enter__ = lambda x: x
     cls.__exit__ = lambda x, a, b, c: None
+
+
+def create_test_user(
+        username='testuser',
+        password=uuid.uuid4(),
+        email='testuser@snommoc.org'
+) -> User:
+    user = User.objects.create(
+        username=username,
+        password=password,
+        email=email)
+    user.save()
+    return user

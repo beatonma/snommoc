70cc946ae4772d77e0ac725e75a47a3220dbb0c9
Michael Beaton <beatonma@gmail.com>
2019-06-16 16:32:27 +0100

    A dict with value for API key is now provided to the receiving method in the 'query' kwarg

--- api/tests/views.py
+++ api/tests/views.py
@@ -3,6 +3,7 @@ from functools import wraps
 from django.http import HttpResponse
 from django.views import View
 
+from api import contract as api_contract
 from api.models import ApiKey
 from api.views.decorators.api_key_required import api_key_required
 from basetest import test_settings_default
@@ -16,6 +17,9 @@ class ExampleView(View):
 
 
 def with_api_key(f, key=test_settings_default.TEST_API_KEY, **kwargs):
+    """Decorate TestCase setUp methods with @with_api_key.
+    setUp will be called with a kwarg called 'query' which includes
+    a key parameter for making test calls to the api."""
     @wraps(f)
     def create_test_api_key(testcase):
         ApiKey.objects.create(
@@ -23,6 +27,11 @@ def with_api_key(f, key=test_settings_default.TEST_API_KEY, **kwargs):
             enabled=True,
             key=key
         ).save()
-        f(testcase, **kwargs)
+        f(
+            testcase,
+            query={
+                api_contract.API_KEY: key,
+            },
+            **kwargs)
 
     return create_test_api_key

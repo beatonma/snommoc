3931065ea956898eac8be2f9c61f6e804a1e32ce
Michael Beaton <beatonma@gmail.com>
2019-06-23 16:55:18 +0100

    URLs for lda API endpoints

---
+++ crawlers/parliamentdotuk/tasks/lda/endpoints.py
@@ -0,0 +1,6 @@
+CONSTITUENCIES_BASE_URL = 'http://lda.data.parliament.uk/constituencies.json'
+COMMONS_MEMBERS_BASE_URL = 'http://lda.data.parliament.uk/commonsmembers.json'
+
+PARAM_PAGE_SIZE = '_pageSize'
+PARAM_PAGE = '_page'
+MAX_PAGE_SIZE = 500

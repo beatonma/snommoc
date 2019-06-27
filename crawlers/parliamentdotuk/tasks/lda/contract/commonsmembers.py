8e687e85b6bd1c45fd022df1eb04a03754a98573
Michael Beaton <beatonma@gmail.com>
2019-06-27 21:52:05 +0100

    First commit. Keys used in LDA api

---
+++ crawlers/parliamentdotuk/tasks/lda/contract/commonsmembers.py
@@ -0,0 +1,12 @@
+"""JSON keys as used in http://lda.data.parliament.uk/commonsmembers.json
+"""
+NAME_FULL = 'fullName'
+NAME_FIRST = 'givenName'
+NAME_LAST = 'familyName'
+NAME_MIDDLE = 'additionalName'
+CONSTITUENCY = 'constituency'
+GENDER = 'gender'
+PARTY = 'party'
+TWITTER = 'twitter'
+HOMEPAGE = 'homePage'
+ABOUT = '_about'
---
+++ crawlers/parliamentdotuk/tasks/lda/contract/constituencies.py
@@ -0,0 +1,9 @@
+"""JSON keys as used in http://lda.data.parliament.uk/constituencies.json
+"""
+
+NAME = 'label'
+TYPE = 'constituencyType'
+DATE_STARTED = 'startedDate'
+DATE_ENDED = 'endedDate'
+ORDINANCE_SURVEY_NAME = 'osName'
+GSS_CODE = 'gssCode'

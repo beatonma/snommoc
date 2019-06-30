196f0aabf858517214d51c36b126ea9d6dae3fec
Michael Beaton <beatonma@gmail.com>
2019-06-30 19:00:23 +0100

    First commit

---
+++ crawlers/parliamentdotuk/__init__.py
@@ -0,0 +1,5 @@
+"""
+Points of interest in the API:
+- Election Results http://explore.data.parliament.uk/?learnmore=Election%20Results
+- Elections: http://lda.data.parliament.uk/elections.json
+"""

bd667091848fe4543f18883c7bb1196e105fc686
Michael Beaton <beatonma@gmail.com>
2019-09-01 12:18:31 +0100

    Added verbose_name_plural for admin

--- repository/models/party.py
+++ repository/models/party.py
@@ -7,8 +7,8 @@ from api import contract
 
 class Party(models.Model):
     name = models.CharField(max_length=32)
-    short_name = models.CharField(max_length=16)
-    long_name = models.CharField(max_length=64)
+    short_name = models.CharField(max_length=16, default=name)
+    long_name = models.CharField(max_length=64, default=name)
 
     @classmethod
     def create(cls, name, short_name, long_name) -> 'Party':
@@ -22,3 +22,6 @@ class Party(models.Model):
             contract.PARTY_SHORT: self.short_name,
             contract.PARTY_LONG: self.long_name,
         }
+
+    class Meta:
+        verbose_name_plural = 'Parties'

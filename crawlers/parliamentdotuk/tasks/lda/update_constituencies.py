0a94e2aaa4a1ff1f6b201d51694e524756cf76a9
Michael Beaton <beatonma@gmail.com>
2019-09-01 12:17:41 +0100

    Replaced string literals with constituencies_contract constants

--- crawlers/parliamentdotuk/tasks/lda/update_constituencies.py
+++ crawlers/parliamentdotuk/tasks/lda/update_constituencies.py
@@ -4,7 +4,7 @@ from typing import Optional
 from celery import shared_task
 
 from crawlers.parliamentdotuk.tasks.lda import endpoints
-from notifications.models import TaskNotification
+from crawlers.parliamentdotuk.tasks.lda.contract import constituencies as constituencies_contract
 from repository.models import Constituency
 from .util import (
     get_value,
@@ -15,23 +15,20 @@ log = logging.getLogger(__name__)
 
 
 @shared_task
-def update_constituencies(report: bool = True) -> None:
-    """
-    :param report: Create a TaskNotification with the results of this task
-    """
+def update_constituencies() -> None:
     def build_constituency(json_data) -> Optional[str]:
-        if json_data.get('endedDate'):
-            log.debug(f'Skipping obsolete constituency: {get_value(json_data, "label")}')
+        if json_data.get(constituencies_contract.DATE_ENDED):
+            # log.debug(f'Skipping obsolete constituency: {get_value(json_data, "label")}')
             return
 
-        name = get_value(json_data, 'label')
+        name = get_value(json_data, constituencies_contract.NAME)
         constituency, created = Constituency.objects.update_or_create(
             name=name,
             defaults={
                 'name': name,
-                'gss_code': get_value(json_data, 'gssCode'),
-                'ordinance_survey_name': get_value(json_data, 'osName'),
-                'constituency_type': get_value(json_data, 'constituencyType')
+                'gss_code': get_value(json_data, constituencies_contract.GSS_CODE),
+                'ordinance_survey_name': get_value(json_data, constituencies_contract.ORDINANCE_SURVEY_NAME),
+                'constituency_type': get_value(json_data, constituencies_contract.TYPE)
             }
         )
         constituency.save()

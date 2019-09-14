67439be6457c73298268919da2c21b1fea69f3bc
Michael Beaton <beatonma@gmail.com>
2019-09-14 14:51:41 +0100

    Replaced separate PoliticalInterest, CountryOfInterest clasess with single Interest class with 'classification' field

--- repository/models/interests.py
+++ repository/models/interests.py
@@ -2,32 +2,40 @@ from typing import List
 
 from django.db import models
 
-from repository.models import GenericPersonForeignKeyMixin
+from repository.models.people import GenericPersonForeignKeyMixin
 
 
-class PoliticalInterest(GenericPersonForeignKeyMixin, models.Model):
+INTEREST_CLASSIFICATION_GENERIC = 'political'
+INTEREST_CLASSIFICATION_COUNTRY = 'country'
+
+
+class Interest(GenericPersonForeignKeyMixin, models.Model):
     description = models.CharField(max_length=48)
+    classification = models.CharField(
+        max_length=16,
+        choices=[
+            (INTEREST_CLASSIFICATION_GENERIC, INTEREST_CLASSIFICATION_GENERIC),
+            (INTEREST_CLASSIFICATION_COUNTRY, INTEREST_CLASSIFICATION_COUNTRY),
+        ],
+        default='political',
+    )
 
     @classmethod
     def create(cls, person, interests: List[str]):
         for description in interests:
-            PoliticalInterest.objects.create(
+            Interest.objects.create(
                 person=person,
-                description=description).save()
+                description=description
+            ).save()
+
+    @classmethod
+    def create_countries(cls, person, countries: List[str]):
+        for country in countries:
+            Interest.objects.create(
+                person=person,
+                description=country,
+                classification=INTEREST_CLASSIFICATION_COUNTRY,
+            ).save()
 
     def __str__(self):
         return f'{self.description}'
-
-
-class CountryOfInterest(GenericPersonForeignKeyMixin, models.Model):
-    country = models.CharField(max_length=48)
-
-    @classmethod
-    def create(cls, person, interests: List[str]):
-        for country in interests:
-            CountryOfInterest.objects.create(
-                person=person,
-                country=country).save()
-
-    def __str__(self):
-        return f'{self.country}'

17c6122bd52954c14f2634217662ca7367da7120
Michael Beaton <beatonma@gmail.com>
2019-09-14 19:36:56 +0100

    Added explicit choices to constituency_type

--- repository/models/constituency.py
+++ repository/models/constituency.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Constituency(models.Model):
     name = models.CharField(max_length=64)
-    mp = models.OneToOneField('Mp', on_delete=models.CASCADE, null=True)
+    mp = models.OneToOneField('Mp', on_delete=models.SET_NULL, null=True)
 
     ordinance_survey_name = models.CharField(max_length=64, null=True)
     gss_code = models.CharField(
@@ -11,6 +11,10 @@ class Constituency(models.Model):
         help_text='Government Statistical Service ID')
     constituency_type = models.CharField(
         max_length=10, null=True,
+        choices=[
+            ('county', 'County'),
+            ('borough', 'Borough'),
+        ],
         help_text='Borough, county...')
 
     def __str__(self):

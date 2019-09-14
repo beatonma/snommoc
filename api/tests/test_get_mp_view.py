af46d8aa46ffbbe64a6136330453e482d9591358
Michael Beaton <beatonma@gmail.com>
2019-09-14 16:05:38 +0100

    Use api.endpoints for reverse lookups

--- api/tests/test_get_mp_view.py
+++ api/tests/test_get_mp_view.py
@@ -1,21 +1,28 @@
 import json
+import logging
 
 from django.http import JsonResponse
 from django.urls import reverse
 
 from api import contract as api_contract
+from api import endpoints as api_endpoints
 from api.tests.views import with_api_key
-from api.views.people import (
-    VIEW_GET_ALL_MPS,
-    VIEW_GET_MP,
-)
+# from api.views.people import (
+#     VIEW_GET_ALL_MPS,
+#     VIEW_GET_MP,
+# )
 from basetest.testcase import LocalTestCase
 from repository.models import (
-    Party,
     Constituency,
     Mp,
+    Party,
 )
 from repository.tests import values
+
+log = logging.getLogger(__name__)
+
+
+# TODO UPDATE VIEWS AND TESTS FOR DJANGO REST FRAMEWORK
 
 
 class MpViewTest(LocalTestCase):
@@ -29,7 +36,7 @@ class MpViewTest(LocalTestCase):
 
         self.mp = Mp.create(
             name=values.EXAMPLE_NAME,
-            aliases=values.EXAMPLE_ALIASES,
+            # aliases=values.EXAMPLE_ALIASES,
             puk=values.EXAMPLE_PUK_ID,
             twfy=values.EXAMPLE_TWFY_ID,
             party=values.PARTY_NAME,
@@ -52,40 +59,42 @@ class MpViewTest(LocalTestCase):
 class GetMpTest(MpViewTest):
     def test_get_mp_view_with_puk_id(self):
         response = self.client.get(
-            reverse(VIEW_GET_MP),
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP)),
             data={
                 **self.query,
                 api_contract.PARLIAMENTDOTUK_ID: values.EXAMPLE_PUK_ID
             })
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.content_type, 'application/json')
+        # self.assertIsInstance(response, JsonResponse)
 
         data = json.loads(response.content)
         self.assertEqual(data, values.MP_STRUCTURE)
 
     def test_get_mp_view_with_twfy_id(self):
         response = self.client.get(
-            reverse(VIEW_GET_MP),
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP)),
             data={
                 **self.query,
                 api_contract.THEYWORKFORYOU_ID: values.EXAMPLE_TWFY_ID
             })
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response, JsonResponse)
+        # self.assertIsInstance(response, JsonResponse)
 
         data = json.loads(response.content)
         self.assertEqual(data, values.MP_STRUCTURE)
 
     def test_get_mp_view_with_puk_and_ids(self):
         response = self.client.get(
-            reverse(VIEW_GET_MP),
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP)),
             data={
                 **self.query,
                 api_contract.THEYWORKFORYOU_ID: values.EXAMPLE_TWFY_ID,
                 api_contract.PARLIAMENTDOTUK_ID: values.EXAMPLE_PUK_ID
             })
+        log.info(response.url)
         self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response, JsonResponse)
+        # self.assertIsInstance(response, JsonResponse)
 
         data = json.loads(response.content)
         self.assertEqual(data, values.MP_STRUCTURE)
@@ -93,14 +102,14 @@ class GetMpTest(MpViewTest):
     def test_get_mp_view_with_no_id(self):
         """No id provided -> HttpResponseBadRequest"""
         response = self.client.get(
-            reverse(VIEW_GET_MP),
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP)),
             data=self.query
         )
         self.assertEqual(response.status_code, 400)
 
     def test_get_mp_view__without_api_key__should_return_http400(self):
         response = self.client.get(
-            reverse(VIEW_GET_MP)
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP))
         )
         self.assertEqual(response.status_code, 400)
 
@@ -108,7 +117,7 @@ class GetMpTest(MpViewTest):
 class GetAllMPsTest(MpViewTest):
     def test_get_all_mps_view__is_correct(self):
         response = self.client.get(
-            reverse(VIEW_GET_ALL_MPS),
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP)),
             data=self.query
         )
         self.assertEqual(response.status_code, 200)
@@ -119,6 +128,6 @@ class GetAllMPsTest(MpViewTest):
 
     def test_get_all_mps_view__without_api_key__should_return_http400(self):
         response = self.client.get(
-            reverse(VIEW_GET_ALL_MPS)
+            reverse(api_endpoints.endpoint_list(api_endpoints.MP))
         )
         self.assertEqual(response.status_code, 400)

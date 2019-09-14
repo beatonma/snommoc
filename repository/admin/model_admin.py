4cf876f7c51dcf98096e21bb2752dc6a8f08a8b9
Michael Beaton <beatonma@gmail.com>
2019-09-14 19:40:14 +0100

    Updated admin after restructuring models to use Person foreignkey

--- repository/admin/model_admin.py
+++ repository/admin/model_admin.py
@@ -1,68 +1,39 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import (
-    GenericStackedInline,
-    GenericTabularInline,
-)
+from django.contrib.admin import TabularInline
 
-from repository.models import (
-    Constituency,
-    Interest,
-)
 from repository.models.contact_details import (
-    PersonalLinks,
+    Links,
     WebLink,
 )
+
 from repository.models import (
     Mp,
     Party,
+    Constituency,
 )
-# from repository.models.interests import (
-#     CountryOfInterest,
-#     PoliticalInterest,
-# )
 
 
-class PersonalLinksInline(GenericStackedInline):
-    model = PersonalLinks
-    ct_field = 'person_content_type'
-    ct_fk_field = 'person_id'
-    max_num = 1
+from repository.models.interests import Interest
+from repository.models.people import Person
 
 
-class InterestInline(GenericTabularInline):
+class InterestInline(TabularInline):
     model = Interest
-    ct_field = 'person_content_type'
-    ct_fk_field = 'person_id'
-
-
-# class CountryOfInterestInline(GenericTabularInline):
-#     model = CountryOfInterest
-#     ct_field = 'person_content_type'
-#     ct_fk_field = 'person_id'
-#
-#
-# class PoliticalInterestInline(GenericTabularInline):
-#     model = PoliticalInterest
-#     ct_field = 'person_content_type'
-#     ct_fk_field = 'person_id'
 
 
 @admin.register(*[
     Constituency,
     Party,
+    Links,
     WebLink,
-    # CountryOfInterest,
-    # PoliticalInterest,
+    Mp,
 ])
 class DefaultAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Mp)
-class MpAdmin(DefaultAdmin):
+@admin.register(Person)
+class PersonAdmin(DefaultAdmin):
     inlines = [
-        PersonalLinksInline,
         InterestInline,
-        # PoliticalInterestInline,
-        # CountryOfInterestInline,
     ]

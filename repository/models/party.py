7e6035d56eaae2ab0cf8c21ce665b2232d2b2bc9
Michael Beaton <beatonma@gmail.com>
2019-11-02 17:39:57 +0000

    Removed serialisation function, made redundant by drf

--- repository/models/party.py
+++ repository/models/party.py
@@ -1,27 +1,20 @@
-from typing import Dict
-
 from django.db import models
-
-from api import contract
 
 
 class Party(models.Model):
-    name = models.CharField(max_length=32)
-    short_name = models.CharField(max_length=16, default=name)
-    long_name = models.CharField(max_length=64, default=name)
+    name = models.CharField(max_length=32, unique=True)
+    short_name = models.CharField(max_length=16, unique=True)
+    long_name = models.CharField(
+        max_length=64,
+        unique=True,
+        help_text='Official name',
+    )
 
     @classmethod
     def create(cls, name, short_name, long_name) -> 'Party':
         party = cls(name=name, short_name=short_name, long_name=long_name)
         party.save()
         return party
-
-    def to_json(self) -> Dict:
-        return {
-            contract.PARTY: self.name,
-            contract.PARTY_SHORT: self.short_name,
-            contract.PARTY_LONG: self.long_name,
-        }
 
     class Meta:
         verbose_name_plural = 'Parties'

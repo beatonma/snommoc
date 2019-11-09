ca7eef80b07e2060ab3926967621547022aed661
Michael Beaton <beatonma@gmail.com>
2019-11-09 19:53:37 +0000

    Added additional_name, gender fields. No plan for these yet but they're available so might as well take them.

--- repository/models/person.py
+++ repository/models/person.py
@@ -22,6 +22,19 @@ class Person(models.Model):
         help_text='Last name',
         null=True)
 
+    additional_name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        help_text='Middle name(s)',
+        blank=True,
+        null=True)
+
+    gender = models.CharField(
+        max_length=16,
+        default=None,
+        null=True,
+        blank=True
+    )
+
     def __str__(self):
         return self.name
 

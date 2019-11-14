ee509fc1dcabbef13a83687e828f4192821d6307
Michael Beaton <beatonma@gmail.com>
2019-11-14 21:30:51 +0000

    Implemented basic dashboard for viewing repository data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from dashboard.views.dashboard import DashboardView
+from dashboard.views.dashboard import (
+    ConstituencyDashboardView,
+    DashboardView,
+    MpDashboardView,
+    PartyDashboardView,
+)
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('mps/', MpDashboardView.as_view(), name='dashboard-mps'),
+    path('mps/<str:requirement>/', MpDashboardView.as_view(), name='dashboard-mps'),
+    path('constituencies/', ConstituencyDashboardView.as_view(), name='dashboard-constituencies'),
+    path('parties/', PartyDashboardView.as_view(), name='dashboard-parties'),
 ]
--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -1,8 +1,18 @@
-from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
 
 from notifications.models import TaskNotification
+from repository.models import (
+    Mp,
+    Constituency,
+    Party,
+)
+
+
+def with_unread_notifications(context: dict):
+    context['notifications'] = TaskNotification.objects.filter(read=False)
+    return context
 
 
 class DashboardView(View):
@@ -10,7 +20,49 @@ class DashboardView(View):
         return render(
             request,
             'staff-dashboard.html',
-            {
-                'notifications': TaskNotification.objects.filter(read=False)
-            }
+            with_unread_notifications({
+                'mp_count': Mp.objects.count(),
+                'constituency_count': Constituency.objects.count(),
+                'party_count': Party.objects.count(),
+            })
+        )
+
+
+class MpDashboardView(View):
+    def get(self, request, *args, **kwargs):
+        if kwargs.get('requirement') == 'active':
+            mps = Mp.objects.prefetch_related('party') \
+                .order_by('person__family_name') \
+                .exclude(constituency__isnull=True)
+        else:
+            mps = Mp.objects.prefetch_related('party') \
+                .order_by('person__family_name')
+        return render(
+            request,
+            'dashboard-all-mps.html',
+            with_unread_notifications({
+                'items': mps,
+            })
+        )
+
+
+class ConstituencyDashboardView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'dashboard-all-constituencies.html',
+            with_unread_notifications({
+                'items': Constituency.objects.order_by('name'),
+            })
+        )
+
+
+class PartyDashboardView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'dashboard-all-parties.html',
+            with_unread_notifications({
+                'items': Party.objects.order_by('name'),
+            })
         )

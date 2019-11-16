22f9a872bbfe475f83744a65530cebe50c0782e9
Michael Beaton <beatonma@gmail.com>
2019-11-16 18:41:35 +0000

    Added urls for mp/constituency update actions. Added urls for passing requirements to Views for MP, Party, Constituency lists

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from dashboard.views.actions import (
+    UpdateConstituenciesView,
+    UpdateMpsView,
+)
 from dashboard.views.dashboard import (
     ConstituencyDashboardView,
     DashboardView,
@@ -12,5 +16,12 @@ urlpatterns = [
     path('mps/', MpDashboardView.as_view(), name='dashboard-mps'),
     path('mps/<str:requirement>/', MpDashboardView.as_view(), name='dashboard-mps'),
     path('constituencies/', ConstituencyDashboardView.as_view(), name='dashboard-constituencies'),
+    path('constituencies/<str:requirement>/', ConstituencyDashboardView.as_view(), name='dashboard-constituencies'),
     path('parties/', PartyDashboardView.as_view(), name='dashboard-parties'),
+    path('parties/<str:requirement>/', PartyDashboardView.as_view(), name='dashboard-parties'),
+
+    path('actions/update-constituencies/', UpdateConstituenciesView.as_view(), name='action-update-constituencies'),
+    path('actions/update-constituencies/<str:requirement>/', UpdateConstituenciesView.as_view(), name='action-update-constituencies'),
+    path('actions/update-mps/', UpdateMpsView.as_view(), name='action-update-mps'),
+    path('actions/update-mps/<str:requirement>/', UpdateMpsView.as_view(), name='action-update-mps'),
 ]

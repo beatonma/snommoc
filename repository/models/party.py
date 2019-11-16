591b8fda9a288528beed4c888161bcf6d4358ee8
Michael Beaton <beatonma@gmail.com>
2019-11-16 18:40:29 +0000

    Implemented mp_population property

--- repository/models/party.py
+++ repository/models/party.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import DO_NOTHING
 
 from repository.models.mixins import WikipediaMixin
 
@@ -23,6 +24,11 @@ class Party(WikipediaMixin, models.Model):
     homepage = models.URLField(null=True, blank=True)
     year_founded = models.PositiveSmallIntegerField(default=0)
 
+    @property
+    def mp_population(self) -> int:
+        """Returns number of MPs associated with this party"""
+        return self.objects.count()
+
     @classmethod
     def create(cls, name, short_name, long_name) -> 'Party':
         party = cls(name=name, short_name=short_name, long_name=long_name)
@@ -34,3 +40,31 @@ class Party(WikipediaMixin, models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PartyTheme(models.Model):
+    TEXT_COLOR_OPTIONS = [
+        ('light', 'light'),
+        ('dark', 'dark'),
+    ]
+
+    party = models.OneToOneField(
+        Party,
+        on_delete=DO_NOTHING,
+        null=True,
+        blank=True,
+        related_name='theme',
+    )
+
+    primary_color = models.CharField(max_length=6, help_text='Hex color code')
+    accent_color = models.CharField(max_length=6, help_text='Hex color code')
+    primary_text_color = models.CharField(
+        max_length=5,
+        choices=TEXT_COLOR_OPTIONS,
+        help_text='Color for text that overlays primary_color',
+    )
+    accent_text_color = models.CharField(
+        max_length=5,
+        choices=TEXT_COLOR_OPTIONS,
+        help_text='Color for text that overlays accent_color',
+    )

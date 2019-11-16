b729150079b2e5c0712f0f263032e59987fd1042
Michael Beaton <beatonma@gmail.com>
2019-11-16 18:38:48 +0000

    Implemented qs_get_or_none

--- repository/models/util/queryset.py
+++ repository/models/util/queryset.py
@@ -1,5 +1,15 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+
 def get_or_none(classmodel, **kwargs):
     try:
         return classmodel.objects.get(**kwargs)
     except classmodel.DoesNotExist:
+        return None
+
+
+def qs_get_or_none(queryset, **kwargs):
+    try:
+        return queryset.get(**kwargs)
+    except ObjectDoesNotExist:
         return None

ff6db65d47234f41a8eec35e07acdca09a81495e
Michael Beaton <beatonma@gmail.com>
2019-11-16 17:21:57 +0000

    Implemented views for updating Constituencies and MPs

---
+++ dashboard/views/actions.py
@@ -0,0 +1,29 @@
+"""
+
+"""
+
+import logging
+
+from django.http import HttpResponse
+
+from crawlers.parliamentdotuk.tasks import (
+    update_constituencies,
+    update_mps,
+)
+from dashboard.views.dashboard import BaseDashboardView
+
+log = logging.getLogger(__name__)
+
+
+class UpdateConstituenciesView(BaseDashboardView):
+    def get(self, request, *args, **kwargs):
+        follow_pagination = kwargs.get('requirement') != 'debug'
+        update_constituencies(follow_pagination=follow_pagination)
+        return HttpResponse('OK')
+
+
+class UpdateMpsView(BaseDashboardView):
+    def get(self, request, *args, **kwargs):
+        follow_pagination = kwargs.get('requirement') != 'debug'
+        update_mps(follow_pagination=follow_pagination)
+        return HttpResponse('OK')

74acc1e98d26bc3d25abc420deb048522eda275b
Michael Beaton <beatonma@gmail.com>
2019-11-16 18:37:36 +0000

    Added founded_year, homepage fields

--- repository/tasks/init_parties.py
+++ repository/tasks/init_parties.py
@@ -13,31 +13,42 @@ PARTY_DEFINITIONS = [
     # (common_name, official_name, abbreviation)
 
     # Parties that currently (11/2019) have seats in the Commons
-    ('Conservative', 'Conservative and Unionist Party', 'Con'),
-    ('Labour', 'Labour Party', 'Lab'),
-    ('SNP', 'Scottish National Party', 'SNP'),
-    ('Independent', 'Independent', 'Indie'),
-    ('Lib Dem', 'Liberal Democrats', 'LD'),
-    ('DUP', 'Democratic Unionist Party', 'DUP'),
-    ('Sinn Féin', 'Sinn Féin', 'SF'),
-    ('Change UK', 'The Independent Group for Change', 'TIG'),
-    ('Plaid Cymru', 'Plaid Cymru – The Party of Wales', 'Plaid'),
-    ('Green Party', 'Green Party of England and Wales', 'Green'),
+    ('Conservative', 'Conservative and Unionist Party', 'Con', 1834, 'https://www.conservatives.com/'),
+    ('Labour', 'Labour Party', 'Lab', 1900, 'https://labour.org.uk/'),
+    ('SNP', 'Scottish National Party', 'SNP', 1934, 'https://www.snp.org/'),
+    ('Independent', 'Independent', 'Indie', 0, None),
+    ('Lib Dem', 'Liberal Democrats', 'LD', 1988, 'https://www.libdems.org.uk/'),
+    ('DUP', 'Democratic Unionist Party', 'DUP', 1971, 'https://www.mydup.com/'),
+    ('Sinn Féin', 'Sinn Féin', 'SF', 1905, 'https://www.sinnfein.ie/'),
+    ('Change UK', 'The Independent Group for Change', 'TIG', 2019, 'https://voteforchange.uk/'),
+    ('Plaid Cymru', 'Plaid Cymru – The Party of Wales', 'Plaid', 1925, 'https://www.plaid.cymru/'),
+    ('Green Party', 'Green Party of England and Wales', 'Green', 1990, 'https://www.greenparty.org.uk/'),
 
     # Parties that had seats in the previous Parliament but not now
-    ('SDLP', 'Social Democratic and Labour Party', 'SDLP'),
-    ('UUP', 'Ulster Unionist Party', 'UUP'),
-    ('UKIP', 'UK Independence Party', 'UKIP'),
+    ('SDLP', 'Social Democratic and Labour Party', 'SDLP', 1970, 'https://www.sdlp.ie/'),
+    ('UUP', 'Ulster Unionist Party', 'UUP', 1905, 'https://www.uup.org/'),
+    ('UKIP', 'UK Independence Party', 'UKIP', 1993, 'https://www.ukip.org/'),
 
     # Other entities
-    ('Speaker', 'Speaker', 'Speaker'),
+    ('Speaker', 'Speaker', 'Speaker', 1377, 'https://www.parliament.uk/business/commons/the-speaker/'),
 ]
 
 
 def init_parties():
-    for (name, long_name, abbreviation) in PARTY_DEFINITIONS:
-        Party.objects.create(
+    for (
+            name,
+            long_name,
+            abbreviation,
+            year_founded,
+            homepage
+    ) in PARTY_DEFINITIONS:
+        Party.objects.update_or_create(
             name=name,
-            long_name=long_name,
-            short_name=abbreviation
-        ).save()
+            defaults={
+                'name': name,
+                'long_name': long_name,
+                'short_name': abbreviation,
+                'year_founded': year_founded,
+                'homepage': homepage
+            }
+        )

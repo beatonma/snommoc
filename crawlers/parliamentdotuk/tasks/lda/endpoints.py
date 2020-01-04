a03ebf6fec5660d979ee3838a5d79e32b6fe2fae
Michael Beaton <beatonma@gmail.com>
2020-01-04 01:07:51 +0000

    http -> https

--- crawlers/parliamentdotuk/tasks/lda/endpoints.py
+++ crawlers/parliamentdotuk/tasks/lda/endpoints.py
@@ -1,5 +1,5 @@
-CONSTITUENCIES_BASE_URL = 'http://lda.data.parliament.uk/constituencies.json'
-COMMONS_MEMBERS_BASE_URL = 'http://lda.data.parliament.uk/commonsmembers.json'
+CONSTITUENCIES_BASE_URL = 'https://lda.data.parliament.uk/constituencies.json'
+COMMONS_MEMBERS_BASE_URL = 'https://lda.data.parliament.uk/commonsmembers.json'
 
 PARAM_PAGE_SIZE = '_pageSize'
 PARAM_PAGE = '_page'

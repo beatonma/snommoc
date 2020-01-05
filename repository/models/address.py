3da2e847593aaec7e0ff905008979f306f967350
Michael Beaton <beatonma@gmail.com>
2020-01-05 22:23:11 +0000

    Implemented PhysicalAddress, WebAddress

---
+++ repository/models/address.py
@@ -0,0 +1,39 @@
+"""
+
+"""
+
+import logging
+
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from repository.models.mixins import (
+    BaseModel,
+    PersonMixin,
+)
+
+log = logging.getLogger(__name__)
+
+
+PHONE_NUMBER_REGION = 'GB'
+
+
+class PhysicalAddress(PersonMixin, BaseModel):
+    description = models.CharField(
+        max_length=32,
+        help_text='What kind of address is this? e.g Parliamentary, Constituency...'
+    )
+    address = models.CharField(max_length=256)
+    postcode = models.CharField(max_length=10, null=True, blank=True)
+    phone = PhoneNumberField(null=True, blank=True)
+    fax = PhoneNumberField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+
+class WebAddress(PersonMixin, BaseModel):
+    description = models.CharField(
+        max_length=32,
+        help_text='What kind of address is this? e.g Twitter, personal site...'
+    )
+
+    url = models.URLField()

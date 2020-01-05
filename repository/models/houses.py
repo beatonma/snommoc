2e4f7bb113bcf3d583491dd8d5010d882ef18569
Michael Beaton <beatonma@gmail.com>
2020-01-05 00:54:11 +0000

    Added unique constraint to house.name

--- repository/models/houses.py
+++ repository/models/houses.py
@@ -19,7 +19,10 @@ HOUSE_OF_LORDS = 'Lords'
 
 
 class House(BaseModel):
-    name = models.CharField(max_length=16)
+    name = models.CharField(
+        max_length=16,
+        unique=True,
+    )
 
     def __str__(self):
         return self.name

7075f994547008cefc54f9072cf20ffe57681fdb
Michael Beaton <beatonma@gmail.com>
2020-01-05 00:56:56 +0000

    Added wikipedia pages. Updated current parties - added APNI

--- repository/tasks/init_parties.py
+++ repository/tasks/init_parties.py
@@ -10,27 +10,29 @@ log = logging.getLogger(__name__)
 
 
 PARTY_DEFINITIONS = [
-    # (common_name, official_name, abbreviation)
+    # UPDATED January 2020
+    # (common_name, official_name, abbreviation, year_founded, homepage, wikipedia)
 
-    # Parties that currently (11/2019) have seats in the Commons
-    ('Conservative', 'Conservative and Unionist Party', 'Con', 1834, 'https://www.conservatives.com/'),
-    ('Labour', 'Labour Party', 'Lab', 1900, 'https://labour.org.uk/'),
-    ('SNP', 'Scottish National Party', 'SNP', 1934, 'https://www.snp.org/'),
-    ('Independent', 'Independent', 'Indie', 0, None),
-    ('Lib Dem', 'Liberal Democrats', 'LD', 1988, 'https://www.libdems.org.uk/'),
-    ('DUP', 'Democratic Unionist Party', 'DUP', 1971, 'https://www.mydup.com/'),
-    ('Sinn Féin', 'Sinn Féin', 'SF', 1905, 'https://www.sinnfein.ie/'),
-    ('Change UK', 'The Independent Group for Change', 'TIG', 2019, 'https://voteforchange.uk/'),
-    ('Plaid Cymru', 'Plaid Cymru – The Party of Wales', 'Plaid', 1925, 'https://www.plaid.cymru/'),
-    ('Green Party', 'Green Party of England and Wales', 'Green', 1990, 'https://www.greenparty.org.uk/'),
+    # Parties that currently have seats in the Commons
+    ('Conservative', 'Conservative and Unionist Party', 'Con', 1834, 'https://www.conservatives.com/', 'Conservative_Party_(UK)'),
+    ('Labour', 'Labour Party', 'Lab', 1900, 'https://labour.org.uk/', 'Labour_Party_(UK)'),
+    ('SNP', 'Scottish National Party', 'SNP', 1934, 'https://www.snp.org/', 'Scottish_National_Party'),
+    ('Independent', 'Independent', 'Indie', 0, None, 'Independent_politician'),
+    ('Lib Dem', 'Liberal Democrats', 'LD', 1988, 'https://www.libdems.org.uk/', 'Liberal_Democrats_(UK)'),
+    ('DUP', 'Democratic Unionist Party', 'DUP', 1971, 'https://www.mydup.com/', 'Democratic_Unionist_Party'),
+    ('Sinn Féin', 'Sinn Féin', 'SF', 1905, 'https://www.sinnfein.ie/', 'Sinn_Féin'),
+    ('Plaid Cymru', 'Plaid Cymru – The Party of Wales', 'Plaid', 1925, 'https://www.plaid.cymru/', 'Plaid_Cymru'),
+    ('Green Party', 'Green Party of England and Wales', 'Green', 1990, 'https://www.greenparty.org.uk/', 'Green_Party_of_England_and_Wales'),
+    ('SDLP', 'Social Democratic and Labour Party', 'SDLP', 1970, 'https://www.sdlp.ie/', 'Social_Democratic_and_Labour_Party'),
+    ('Alliance', 'Alliance Party of Northern Ireland', 'APNI', 1970, 'https://www.allianceparty.org/', 'Alliance_Party_of_Northern_Ireland'),
 
-    # Parties that had seats in the previous Parliament but not now
-    ('SDLP', 'Social Democratic and Labour Party', 'SDLP', 1970, 'https://www.sdlp.ie/'),
-    ('UUP', 'Ulster Unionist Party', 'UUP', 1905, 'https://www.uup.org/'),
-    ('UKIP', 'UK Independence Party', 'UKIP', 1993, 'https://www.ukip.org/'),
+    # Other parties of prominence that have held seats in recent years but not now
+    ('Change UK', 'The Independent Group for Change', 'TIG', 2019, 'https://voteforchange.uk/', 'Change_UK'),
+    ('UUP', 'Ulster Unionist Party', 'UUP', 1905, 'https://www.uup.org/', 'Ulster_Unionist_Party'),
+    ('UKIP', 'UK Independence Party', 'UKIP', 1993, 'https://www.ukip.org/', 'UK_Independence_Party'),
 
     # Other entities
-    ('Speaker', 'Speaker', 'Speaker', 1377, 'https://www.parliament.uk/business/commons/the-speaker/'),
+    ('Speaker', 'Speaker', 'Speaker', 1377, 'https://www.parliament.uk/business/commons/the-speaker/', 'Speaker_of_the_House_of_Commons_(United_Kingdom)'),
 ]
 
 
@@ -40,7 +42,8 @@ def init_parties():
             long_name,
             abbreviation,
             year_founded,
-            homepage
+            homepage,
+            wikipedia
     ) in PARTY_DEFINITIONS:
         Party.objects.update_or_create(
             name=name,
@@ -49,6 +52,7 @@ def init_parties():
                 'long_name': long_name,
                 'short_name': abbreviation,
                 'year_founded': year_founded,
-                'homepage': homepage
+                'homepage': homepage,
+                'wikipedia': wikipedia,
             }
         )

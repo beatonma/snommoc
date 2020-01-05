0e4c8181268676343dc1ad1fc5f642d4fff9ded8
Michael Beaton <beatonma@gmail.com>
2020-01-05 18:25:23 +0000

    Added new parsing format to _coerce_to_date - now accepts a dict with 'Day', 'Month', 'Year' keys. Added default return values to _coerce_to_int and _coerce_to_str which will be returned instead of None.
    Implemented ResponseData subclasses for
    - Posts (governmental, parliamentary, opposition)
    - Addresses
    - Interests
    - Experiences
    - BiographyEntries
    
    Added the corresponding get_ methods to MemberBiographyResponseData for retrieving these data.

--- crawlers/parliamentdotuk/tasks/membersdataplatform/mdp_client.py
+++ crawlers/parliamentdotuk/tasks/membersdataplatform/mdp_client.py
@@ -27,6 +27,11 @@ from .contract import (
     party as party_contract,
     status as status_contract,
     committees as committee_contract,
+    posts as posts_contract,
+    addresses as address_contract,
+    biography_entries as bio_entries_contract,
+    interests as interests_contract,
+    experience as experience_contract,
 )
 
 log = logging.getLogger(__name__)
@@ -58,22 +63,15 @@ def _coerce_to_list(obj) -> list:
         return [obj]
 
 
-def _coerce_to_date(value) -> Optional[datetime.date]:
-    try:
-        return dateutil.parser.parse(value).date()
-    except (AttributeError, TypeError, ParserError):
-        return None
-
-
-def _coerce_to_str(value) -> Optional[str]:
-    return str(value) if value else None
-
-
-def _coerce_to_int(value) -> Optional[int]:
+def _coerce_to_int(value, default=None) -> Optional[int]:
     try:
         return int(value)
-    except ValueError:
-        return None
+    except (TypeError, ValueError):
+        return default
+
+
+def _coerce_to_str(value, default=None) -> Optional[str]:
+    return str(value) if value else default
 
 
 def _coerce_to_boolean(value) -> Optional[bool]:
@@ -87,6 +85,27 @@ def _coerce_to_boolean(value) -> Optional[bool]:
             return False
 
     return bool(value)
+
+
+def _coerce_to_date(value) -> Optional[datetime.date]:
+    try:
+        return dateutil.parser.parse(value).date()
+    except (AttributeError, TypeError, ParserError):
+        pass
+
+    if isinstance(value, dict):
+        try:
+            # Default to xmas day - we will use this to assume the day.month
+            # values are missing.
+            return datetime.date(
+                year=_coerce_to_int(value.get('Year')),
+                month=_coerce_to_int(value.get('Month', 12)),
+                day=_coerce_to_int(value.get('Day', 25))
+            )
+        except ValueError:
+            pass
+
+    return None
 
 
 def _is_xml_null(obj: dict) -> bool:
@@ -293,6 +312,107 @@ class CommitteeChairResponseData(ResponseData):
         return self._get_date(committee_contract.END_DATE)
 
 
+class PostResponseData(ResponseData):
+    def get_post_id(self) -> Optional[int]:
+        return self._get_int(posts_contract.PARLIAMENTDOTUK)
+
+    def get_post_name(self) -> Optional[str]:
+        return self._get_str(posts_contract.NAME)
+
+    def get_post_hansard_name(self) -> Optional[str]:
+        return self._get_str(posts_contract.HANSARD_NAME)
+
+    def get_start_date(self) -> Optional[datetime.date]:
+        return self._get_date(posts_contract.START_DATE)
+
+    def get_end_date(self) -> Optional[datetime.date]:
+        return self._get_date(posts_contract.END_DATE)
+
+
+class AddressResponseData(ResponseData):
+    def get_type(self) -> Optional[str]:
+        return self._get_str(address_contract.TYPE)
+
+    def get_is_physical(self) -> Optional[bool]:
+        return self._get_boolean(address_contract.IS_PHYSICAL)
+
+    def get_address(self) -> Optional[str]:
+        address_lines = [self._get_str(key) for key in address_contract.ADDRESS_LINES]
+        address = ', '.join([x for x in address_lines if x])
+        return address
+
+    def get_postcode(self) -> Optional[str]:
+        return self._get_str(address_contract.POSTCODE)
+
+    def get_phone(self) -> Optional[str]:
+        return self._get_str(address_contract.PHONE)
+
+    def get_fax(self) -> Optional[str]:
+        return self._get_str(address_contract.FAX)
+
+    def get_email(self) -> Optional[str]:
+        return self._get_str(address_contract.EMAIL)
+
+
+class InterestResponseData(ResponseData):
+    def get_interest_id(self) -> Optional[int]:
+        return self._get_int(interests_contract.PARLIAMENTDOTUK)
+
+    def get_title(self) -> Optional[str]:
+        return self._get_str(interests_contract.INTEREST_TITLE)
+
+    def get_registered_late(self) -> Optional[bool]:
+        return self._get_boolean(interests_contract.INTEREST_REGISTERD_LATE)
+
+    def get_date_created(self) -> Optional[datetime.date]:
+        return self._get_date(interests_contract.INTEREST_CREATED)
+
+    def get_date_amended(self) -> Optional[datetime.date]:
+        return self._get_date(interests_contract.INTEREST_AMENDED)
+
+    def get_date_deleted(self) -> Optional[datetime.date]:
+        return self._get_date(interests_contract.INTEREST_DELETED)
+
+
+class InterestCategoryResponseData(ResponseData):
+    def get_category_id(self) -> Optional[int]:
+        return self._get_int(interests_contract.PARLIAMENTDOTUK)
+
+    def get_category_name(self) -> Optional[str]:
+        return self._get_int(interests_contract.CATEGORY_NAME)
+
+    def get_interests(self) -> List[InterestResponseData]:
+        return [
+            InterestResponseData(interest) for interest in
+            _coerce_to_list(self.get_value(interests_contract.INTEREST_GROUP_KEY))
+        ]
+
+
+class ExperiencesResponseData(ResponseData):
+    def get_type(self) -> Optional[str]:
+        return self._get_str(experience_contract.TYPE)
+
+    def get_organisation(self) -> Optional[str]:
+        return self._get_str(experience_contract.ORGANISATION)
+
+    def get_title(self) -> Optional[str]:
+        return self._get_str(experience_contract.TITLE)
+
+    def get_start_date(self) -> Optional[datetime.date]:
+        return self._get_date(experience_contract.START_DATE)
+
+    def get_end_date(self) -> Optional[datetime.date]:
+        return self._get_date(experience_contract.END_DATE)
+
+
+class BiographyEntriesResponseData(ResponseData):
+    def get_category(self) -> Optional[str]:
+        return self._get_str(bio_entries_contract.CATEGORY)
+
+    def get_entry(self) -> Optional[str]:
+        return self._get_str(bio_entries_contract.ENTRY)
+
+
 class MemberBiographyResponseData(MemberResponseData):
     def get_basic_info(self) -> 'BasicInfoResponseData':
         return BasicInfoResponseData(self.get_value(basic_contract.BASIC_DETAILS))
@@ -308,6 +428,27 @@ class MemberBiographyResponseData(MemberResponseData):
 
     def get_maiden_speeches(self) -> List['SpeechResponseData']:
         return self._get_response_list(speech_contract.GROUP_KEY, SpeechResponseData)
+
+    def get_goverment_posts(self) -> List['PostResponseData']:
+        return self._get_response_list(posts_contract.GOVERNMENT_GROUP_KEY, PostResponseData)
+
+    def get_parliament_posts(self) -> List['PostResponseData']:
+        return self._get_response_list(posts_contract.PARLIAMENTARY_GROUP_KEY, PostResponseData)
+
+    def get_opposition_posts(self) -> List['PostResponseData']:
+        return self._get_response_list(posts_contract.OPPOSITION_GROUP_KEY, PostResponseData)
+
+    def get_addresses(self) -> List['AddressResponseData']:
+        return self._get_response_list(address_contract.GROUP_KEY, AddressResponseData)
+
+    def get_biography_entries(self) -> List['BiographyEntriesResponseData']:
+        return self._get_response_list(bio_entries_contract.GROUP_KEY, BiographyEntriesResponseData)
+
+    def get_interest_categories(self) -> List['InterestCategoryResponseData']:
+        return self._get_response_list(interests_contract.CATEGORY_GROUP_KEY, InterestCategoryResponseData)
+
+    def get_experiences(self) -> List['ExperiencesResponseData']:
+        return self._get_response_list(experience_contract.GROUP_KEY, ExperiencesResponseData)
 
 
 def update_members(

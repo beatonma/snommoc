b82df0fde67c00e6f0c0a658c0664215627af9d0
Michael Beaton <beatonma@gmail.com>
2020-01-09 20:47:00 +0000

    Removed debugging print()

--- repository/models/util/time.py
+++ repository/models/util/time.py
@@ -9,7 +9,6 @@ log = logging.getLogger(__name__)
 
 def years_since(date: datetime.date, now=datetime.datetime.now()) -> int:
     difference = now.year - date.year
-    print(difference)
     if now.month < date.month:
         difference = difference - 1
     elif now.month == date.month and now.day < date.day:

3cf90d11030d08dfee60099a0d5cafa165b9b2f2
Michael Beaton <beatonma@gmail.com>
2020-01-09 20:36:53 +0000

    Implemented Experience, ExperienceCategory models

---
+++ repository/models/experiences.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+
+import logging
+
+from django.db import models
+
+from repository.models.mixins import (
+    BaseModel,
+    PersonMixin,
+    PeriodMixin,
+)
+
+log = logging.getLogger(__name__)
+
+
+class ExperienceCategory(BaseModel):
+    name = models.CharField(max_length=64)
+
+
+class Experience(PersonMixin, PeriodMixin, BaseModel):
+    category = models.ForeignKey(
+        'ExperienceCategory',
+        on_delete=models.CASCADE,
+    )
+
+    organisation = models.CharField(max_length=64, null=True, blank=True)
+    title = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'{self.category} {self.title}@{self.organisation}'

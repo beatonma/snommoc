6fee852e516a1ddbd497aa0816677463bfec05ee
Michael Beaton <beatonma@gmail.com>
2020-01-09 21:22:52 +0000

    Updated members/mps/lords urls. Added update-parties action.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,18 +3,26 @@ from django.urls import path
 from dashboard.views.actions import (
     UpdateConstituenciesView,
     UpdateMpsView,
+    UpdatePartiesView,
 )
 from dashboard.views.dashboard import (
     ConstituencyDashboardView,
     DashboardView,
     MpDashboardView,
     PartyDashboardView,
+    MemberDashboardView,
+    LordDashboardView,
 )
+
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('members/', MemberDashboardView.as_view(), name='dashboard-members'),
+    path('members/<str:requirement>/', MemberDashboardView.as_view(), name='dashboard-members'),
     path('mps/', MpDashboardView.as_view(), name='dashboard-mps'),
     path('mps/<str:requirement>/', MpDashboardView.as_view(), name='dashboard-mps'),
+    path('lords/', LordDashboardView.as_view(), name='dashboard-lords'),
+    path('lords/<str:requirement>/', LordDashboardView.as_view(), name='dashboard-lords'),
     path('constituencies/', ConstituencyDashboardView.as_view(), name='dashboard-constituencies'),
     path('constituencies/<str:requirement>/', ConstituencyDashboardView.as_view(), name='dashboard-constituencies'),
     path('parties/', PartyDashboardView.as_view(), name='dashboard-parties'),
@@ -22,6 +30,10 @@ urlpatterns = [
 
     path('actions/update-constituencies/', UpdateConstituenciesView.as_view(), name='action-update-constituencies'),
     path('actions/update-constituencies/<str:requirement>/', UpdateConstituenciesView.as_view(), name='action-update-constituencies'),
+
     path('actions/update-mps/', UpdateMpsView.as_view(), name='action-update-mps'),
     path('actions/update-mps/<str:requirement>/', UpdateMpsView.as_view(), name='action-update-mps'),
+
+    path('actions/update-parties/', UpdatePartiesView.as_view(), name='action-update-parties'),
+    path('actions/update-parties/<str:requirement>/', UpdatePartiesView.as_view(), name='action-update-parties'),
 ]

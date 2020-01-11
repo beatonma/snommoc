59207dd4752a218b85c415d19e66d5ffbc12bf12
Michael Beaton <beatonma@gmail.com>
2020-01-11 19:51:47 +0000

    Replaced default admin for TaskNotification

---
+++ notifications/admin.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+import logging
+
+from django.contrib import admin
+
+from notifications.models import TaskNotification
+
+log = logging.getLogger(__name__)
+
+
+@admin.register(TaskNotification)
+class TaskNotificationAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'started_at',
+        'finished_at',
+        'parent',
+        'finished',
+    ]
+
+    ordering = [
+        '-started_at',
+        '-finished_at',
+    ]

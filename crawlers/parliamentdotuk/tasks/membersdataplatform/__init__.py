751a181212297e68b28576d84772cc7546a9e805
Michael Beaton <beatonma@gmail.com>
2020-01-12 15:30:14 +0000

    Implemented update_all_member_data

--- crawlers/parliamentdotuk/tasks/membersdataplatform/__init__.py
+++ crawlers/parliamentdotuk/tasks/membersdataplatform/__init__.py
@@ -3,8 +3,14 @@ See:
 - https://data.parliament.uk/membersdataplatform/referencedata.aspx
 - https://data.parliament.uk/membersdataplatform/memberquery.aspx
 """
-
 import logging
+
+from .active_members import update_active_member_details
+from .all_members import update_all_members_basic_info
 
 log = logging.getLogger(__name__)
 
+
+def update_all_member_data():
+    update_all_members_basic_info()
+    update_active_member_details()

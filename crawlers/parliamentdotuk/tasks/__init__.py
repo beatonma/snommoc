31f930ba001bd39c4f6b96fbf1328d0f84c2a15a
Michael Beaton <beatonma@gmail.com>
2020-01-14 19:57:55 +0000

    Clear/rebuild party data in rebuild_all_data

--- crawlers/parliamentdotuk/tasks/__init__.py
+++ crawlers/parliamentdotuk/tasks/__init__.py
@@ -2,7 +2,9 @@ from repository.models import (
     Constituency,
     # Mp,
     Person,
+    Party,
 )
+from repository.tasks import init_parties
 from .lda.update_constituencies import update_constituencies
 # from .lda.update_mps import update_mps
 from .membersdataplatform import update_all_member_data
@@ -15,11 +17,13 @@ def update_parliament_data():
 
 def clear_parliament_data():
     # Mp.objects.all().delete()
+    Party.objects.all().delete()
     Person.objects.all().delete()
     Constituency.objects.all().delete()
 
 
 def rebuild_all_data():
     clear_parliament_data()
+    init_parties()
     update_constituencies()
     update_all_member_data()

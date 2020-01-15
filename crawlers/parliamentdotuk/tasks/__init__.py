3963292a014d8ee1690becc2eb9ab9064b0d7651
Michael Beaton <beatonma@gmail.com>
2020-01-15 20:52:59 +0000

    Removed unused dev functions

--- crawlers/parliamentdotuk/tasks/__init__.py
+++ crawlers/parliamentdotuk/tasks/__init__.py
@@ -1,22 +1,14 @@
 from repository.models import (
     Constituency,
-    # Mp,
     Person,
     Party,
 )
 from repository.tasks import init_parties
 from .lda.update_constituencies import update_constituencies
-# from .lda.update_mps import update_mps
 from .membersdataplatform import update_all_member_data
 
 
-def update_parliament_data():
-    update_constituencies()
-    # update_mps()
-
-
 def clear_parliament_data():
-    # Mp.objects.all().delete()
     Party.objects.all().delete()
     Person.objects.all().delete()
     Constituency.objects.all().delete()

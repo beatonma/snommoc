9eab51544a27067534c13e5558ca830422ea00e1
Michael Beaton <beatonma@gmail.com>
2020-01-19 13:45:40 +0000

    Split serializers.py into module.

---
+++ api/serializers/__init__.py
@@ -0,0 +1,7 @@
+"""
+
+"""
+
+from .inline import *
+from .detail import *
+from .member_detail import *
---
+++ api/serializers/inline.py
@@ -0,0 +1,81 @@
+"""
+
+"""
+
+import logging
+
+from rest_framework import serializers
+
+from repository.models import (
+    Constituency,
+    Person,
+    Party,
+)
+from api import endpoints
+from repository.models.geography import Town
+
+log = logging.getLogger(__name__)
+
+
+class InlineSerializer(serializers.HyperlinkedModelSerializer):
+    """Return basic data about the object with a link for further
+    details if required."""
+    pass
+
+
+class InlineConstituencySerializer(InlineSerializer):
+    detail_url = serializers.HyperlinkedIdentityField(
+        view_name=f'{endpoints.CONSTITUENCY}-detail',
+        read_only=True
+    )
+
+    class Meta:
+        model = Constituency
+        fields = [
+            'name',
+            'detail_url',
+        ]
+
+
+class InlineTownSerializer(InlineSerializer):
+    town = serializers.CharField(source='name')
+    country = serializers.StringRelatedField(many=False, read_only=True)
+
+    class Meta:
+        model = Town
+        fields = [
+            'town',
+            'country',
+        ]
+
+
+class InlinePartySerializer(InlineSerializer):
+    detail_url = serializers.HyperlinkedIdentityField(
+        view_name=f'{endpoints.PARTY}-detail',
+        read_only=True
+    )
+
+    class Meta:
+        model = Party
+        fields = [
+            'name',
+            'detail_url',
+        ]
+
+
+class InlineMemberSerializer(InlineSerializer):
+    detail_url = serializers.HyperlinkedIdentityField(
+        view_name=f'{endpoints.MEMBER}-detail',
+        read_only=True
+    )
+    party = InlinePartySerializer()
+    constituency = InlineConstituencySerializer()
+
+    class Meta:
+        model = Person
+        fields = [
+            'name',
+            'detail_url',
+            'party',
+            'constituency',
+        ]

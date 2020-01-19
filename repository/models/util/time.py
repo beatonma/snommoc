48524fd040a85bf63941aacb57bd8b2ef810cae0
Michael Beaton <beatonma@gmail.com>
2020-01-19 18:02:13 +0000

    Implemented years_between

--- repository/models/util/time.py
+++ repository/models/util/time.py
@@ -8,17 +8,22 @@ from typing import Optional
 log = logging.getLogger(__name__)
 
 
-def years_since(date: Optional[datetime.date], now=datetime.datetime.now()) -> int:
-    if not date:
+def years_between(date_from: Optional[datetime.date], date_to: Optional[datetime.date]) -> int:
+    """Order is important! We will return zero if date_from is after date_to."""
+    if not date_from or not date_to:
         return 0
 
-    difference = now.year - date.year
-    if now.month < date.month:
+    difference = date_to.year - date_from.year
+    if date_to.month < date_from.month:
         difference = difference - 1
-    elif now.month == date.month and now.day < date.day:
+    elif date_to.month == date_from.month and date_to.day < date_from.day:
         difference = difference - 1
 
     return max(difference, 0)
+
+
+def years_since(date: Optional[datetime.date], now=datetime.datetime.now()) -> int:
+    return years_between(date, now)
 
 
 def is_anniversary(date: Optional[datetime.date], now=datetime.datetime.now()) -> bool:

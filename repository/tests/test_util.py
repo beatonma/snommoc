3b41a8a77a245831ddd9e33720a908e0c7a976c6
Michael Beaton <beatonma@gmail.com>
2020-01-19 17:56:40 +0000

    Added tests for timeutil.years_between

--- repository/tests/test_util.py
+++ repository/tests/test_util.py
@@ -2,6 +2,7 @@
 
 """
 import datetime
+from datetime import date as d
 import logging
 from typing import Optional
 
@@ -13,35 +14,64 @@ log = logging.getLogger(__name__)
 
 class UtilTests(LocalTestCase):
     """"""
+    def test_years_between(self):
+        self.assertEqual(
+            timeutil.years_between(
+                d(year=2000, month=1, day=1),
+                d(year=2001, month=1, day=1)),
+            1)
+        self.assertEqual(
+            timeutil.years_between(
+                d(year=2000, month=1, day=1),
+                d(year=2000, month=12, day=31)),
+            0)
+        self.assertEqual(
+            timeutil.years_between(
+                d(year=2000, month=1, day=1),
+                d(year=2001, month=12, day=31)),
+            1)
+        self.assertEqual(
+            timeutil.years_between(
+                d(year=2000, month=1, day=1),
+                d(year=2005, month=4, day=16)),
+            5)
+
+        # If dates are in wrong order return 0, not a negative number or normalised positive.
+        self.assertEqual(
+            timeutil.years_between(
+                d(year=2005, month=4, day=16),
+                d(year=2000, month=1, day=1)),
+            0)
+
     def test_years_since(self):
-        now = datetime.date(year=2019, month=11, day=17)
+        now = d(year=2019, month=11, day=17)
 
         def _assert_years_since(date: Optional[datetime.date], expected: int):
             self.assertEqual(timeutil.years_since(date, now=now), expected)
 
-        _assert_years_since(datetime.date(year=2019, month=10, day=17), 0)
-        _assert_years_since(datetime.date(year=2019, month=11, day=14), 0)
-        _assert_years_since(datetime.date(year=2018, month=10, day=17), 1)
-        _assert_years_since(datetime.date(year=2018, month=12, day=15), 0)
+        _assert_years_since(d(year=2019, month=10, day=17), 0)
+        _assert_years_since(d(year=2019, month=11, day=14), 0)
+        _assert_years_since(d(year=2018, month=10, day=17), 1)
+        _assert_years_since(d(year=2018, month=12, day=15), 0)
 
-        _assert_years_since(datetime.date(year=1973, month=6, day=10), 46)
-        _assert_years_since(datetime.date(year=1973, month=11, day=17), 46)
-        _assert_years_since(datetime.date(year=1973, month=11, day=28), 45)
-        _assert_years_since(datetime.date(year=1973, month=12, day=8), 45)
+        _assert_years_since(d(year=1973, month=6, day=10), 46)
+        _assert_years_since(d(year=1973, month=11, day=17), 46)
+        _assert_years_since(d(year=1973, month=11, day=28), 45)
+        _assert_years_since(d(year=1973, month=12, day=8), 45)
 
         # Future dates should return zero, not negative values
-        _assert_years_since(datetime.date(year=2020, month=11, day=18), 0)
+        _assert_years_since(d(year=2020, month=11, day=18), 0)
 
         _assert_years_since(None, 0)
 
     def test_is_anniversary(self):
-        now = datetime.date(year=2019, month=11, day=17)
+        now = d(year=2019, month=11, day=17)
 
         def _assert_is_anniversary(year, month, day):
-            self.assertTrue(timeutil.is_anniversary(datetime.date(year=year, month=month, day=day), now))
+            self.assertTrue(timeutil.is_anniversary(d(year=year, month=month, day=day), now))
 
         def _assert_is_not_anniversary(year, month, day):
-            self.assertFalse(timeutil.is_anniversary(datetime.date(year=year, month=month, day=day), now))
+            self.assertFalse(timeutil.is_anniversary(d(year=year, month=month, day=day), now))
 
         _assert_is_not_anniversary(year=2019, month=11, day=16)
         _assert_is_not_anniversary(year=2019, month=11, day=18)

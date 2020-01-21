a020fa49018d3ae331e7b61e3100a33ea1ae6bfb
Michael Beaton <beatonma@gmail.com>
2020-01-21 22:13:55 +0000

    Implemented basic ParliamentarySession model

---
+++ repository/models/session.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+
+import logging
+
+from repository.models.mixins import (
+    PeriodMixin,
+    BaseModel,
+)
+
+log = logging.getLogger(__name__)
+
+
+class ParliamentarySession(PeriodMixin, BaseModel):
+    """A legislative session, usually lasting about a year."""
+    pass

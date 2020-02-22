ec89fc398c3c09035d2f221d56955e09ebabe3be
Michael Beaton <beatonma@gmail.com>
2020-02-22 18:01:33 +0000

    Added ParliamentDotUkMixin to ParliamentarySession

--- repository/models/session.py
+++ repository/models/session.py
@@ -7,11 +7,12 @@ import logging
 from repository.models.mixins import (
     PeriodMixin,
     BaseModel,
+    ParliamentDotUkMixin,
 )
 
 log = logging.getLogger(__name__)
 
 
-class ParliamentarySession(PeriodMixin, BaseModel):
+class ParliamentarySession(ParliamentDotUkMixin, PeriodMixin, BaseModel):
     """A legislative session, usually lasting about a year."""
     pass

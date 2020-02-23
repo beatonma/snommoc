1f97657af71ce760cafbfd1de2d3b2ae6f82a44d
Michael Beaton <beatonma@gmail.com>
2020-02-23 14:46:01 +0000

    Restructured Division/CommonsDivision/LordsDivision models - some fields are only used in one or other of the subclasses. Added unique_together constraints on CommonsDivisionVote/LordsDivisionVote Added __str__ implementations

--- repository/models/divisions.py
+++ repository/models/divisions.py
@@ -9,25 +9,51 @@ from django.db import models
 from repository.models.mixins import (
     BaseModel,
     PersonMixin,
+    ParliamentDotUkMixin,
 )
 
 log = logging.getLogger(__name__)
 
 
-class Division(BaseModel):
+class Division(ParliamentDotUkMixin, BaseModel):
     title = models.CharField(max_length=512)
-    description = models.CharField(max_length=1024)
     date = models.DateField()
 
     session = models.ForeignKey(
         'ParliamentarySession',
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
 
     division_number = models.PositiveSmallIntegerField()
 
     uin = models.CharField(max_length=64)
 
+    ayes = models.PositiveSmallIntegerField(
+        help_text='How many members voted for the motion',
+    )
+    noes = models.PositiveSmallIntegerField(
+        help_text='How many members voted against the motion',
+    )
+
+    @property
+    def passed(self) -> bool:
+        """Return True if the Ayes have it, False otherwise."""
+        return self.ayes > self.noes
+
+    @property
+    def margin(self) -> int:
+        return abs(self.ayes - self.noes)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+
+
+class CommonsDivision(Division):
     deferred_vote = models.BooleanField(
         default=False,
         help_text='A deferred vote is one that is not held immediately '
@@ -35,12 +61,6 @@ class Division(BaseModel):
     )
     abstentions = models.PositiveSmallIntegerField(
         help_text='How many members abstained from voting',
-    )
-    ayes = models.PositiveSmallIntegerField(
-        help_text='How many members voted for the motion',
-    )
-    noes = models.PositiveSmallIntegerField(
-        help_text='How many members voted against the motion',
     )
     did_not_vote = models.PositiveSmallIntegerField()
     errors = models.PositiveSmallIntegerField(
@@ -53,24 +73,11 @@ class Division(BaseModel):
         help_text='How many members were unable to vote due to suspension or expulsion'
     )
 
-    @property
-    def passed(self) -> bool:
-        """Return True if the Ayes have it, False otherwise."""
-        return self.ayes > self.noes
-
-    @property
-    def margin(self) -> int:
-        return abs(self.ayes - self.noes)
-
-    class Meta:
-        abstract = True
-
-
-class CommonsDivision(Division):
-    pass
-
 
 class LordsDivision(Division):
+    description = models.CharField(max_length=1024)
+    whipped_vote = models.BooleanField(default=False)
+
     @property
     def contents(self) -> int:
         """Alias for ayes, as used in the House of Lords."""
@@ -87,6 +94,15 @@ class DivisionVote(PersonMixin, BaseModel):
     no = models.BooleanField(default=False)
     abstention = models.BooleanField(default=False)
 
+    @property
+    def vote_type(self):
+        if self.aye:
+            return 'Aye'
+        elif self.no:
+            return 'No'
+        elif self.abstention:
+            return 'Abstain'
+
     class Meta:
         abstract = True
 
@@ -98,6 +114,14 @@ class CommonsDivisionVote(DivisionVote):
         related_name='votes',
     )
 
+    def __str__(self):
+        return f'{self.person} [{self.vote_type}]: {self.division}'
+
+    class Meta:
+        unique_together = [
+            ['person', 'division'],
+        ]
+
 
 class LordsDivisionVote(DivisionVote):
     division = models.ForeignKey(
@@ -105,3 +129,11 @@ class LordsDivisionVote(DivisionVote):
         on_delete=models.CASCADE,
         related_name='votes',
     )
+
+    def __str__(self):
+        return f'{self.person} [{self.vote_type}]: {self.division}'
+
+    class Meta:
+        unique_together = [
+            ['person', 'division'],
+        ]

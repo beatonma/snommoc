099dfae6c0e02bd558d891b8843f277cde45d4c2
Michael Beaton <beatonma@gmail.com>
2020-02-23 20:05:10 +0000

    Added VOTE_ABSTAINS, VOTE_DID_NOT

--- crawlers/parliamentdotuk/tasks/lda/contract/votes.py
+++ crawlers/parliamentdotuk/tasks/lda/contract/votes.py
@@ -7,4 +7,7 @@ VOTE_CONTENT = 'data.parliament.uk/schema/parl#ContentVote'
 # Lords equivalent to a 'no' vote
 VOTE_NOT_CONTENT = 'data.parliament.uk/schema/parl#NotContentVote'
 
+VOTE_ABSTAINS = 'data.parliament.uk/schema/parl#Abstains'
+VOTE_DID_NOT = 'data.parliament.uk/schema/parl#DidNotVote'
+
 MEMBER = 'member'

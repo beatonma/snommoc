7bde45ce0038dde8ad21b946a216f40847bb99c7
Michael Beaton <beatonma@gmail.com>
2020-02-23 14:38:22 +0000

    Implemented tasks for retrieving divisions data and votes for both Commons and Lords

---
+++ crawlers/parliamentdotuk/tasks/lda/contract/votes.py
@@ -0,0 +1,10 @@
+VOTE_AYE = 'data.parliament.uk/schema/parl#AyeVote'
+VOTE_NO = 'data.parliament.uk/schema/parl#NoVote'
+
+# Lords equivalent to an 'aye' vote
+VOTE_CONTENT = 'data.parliament.uk/schema/parl#ContentVote'
+
+# Lords equivalent to a 'no' vote
+VOTE_NOT_CONTENT = 'data.parliament.uk/schema/parl#NotContentVote'
+
+MEMBER = 'member'

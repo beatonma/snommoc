3bd05115aad32b2a5683cc676af74e48a63d166b
Michael Beaton <beatonma@gmail.com>
2020-03-02 19:53:52 +0000

    Removed 'data.parliament.uk/schema/parl#' prefix from contract values

--- crawlers/parliamentdotuk/tasks/lda/contract/votes.py
+++ crawlers/parliamentdotuk/tasks/lda/contract/votes.py
@@ -1,14 +1,14 @@
-VOTE_AYE = 'data.parliament.uk/schema/parl#AyeVote'
-VOTE_NO = 'data.parliament.uk/schema/parl#NoVote'
+VOTE_AYE = 'AyeVote'
+VOTE_NO = 'NoVote'
 
 # Lords equivalent to an 'aye' vote
-VOTE_CONTENT = 'data.parliament.uk/schema/parl#ContentVote'
+VOTE_CONTENT = 'ContentVote'
 
 # Lords equivalent to a 'no' vote
-VOTE_NOT_CONTENT = 'data.parliament.uk/schema/parl#NotContentVote'
+VOTE_NOT_CONTENT = 'NotContentVote'
 
-VOTE_ABSTAINS = 'data.parliament.uk/schema/parl#Abstains'
-VOTE_DID_NOT = 'data.parliament.uk/schema/parl#DidNotVote'
-VOTE_SUSPENDED_EXPELLED = 'data.parliament.uk/schema/parl#SuspendedOrExpelledVote'
+VOTE_ABSTAINS = 'Abstains'
+VOTE_DID_NOT = 'DidNotVote'
+VOTE_SUSPENDED_EXPELLED = 'SuspendedOrExpelledVote'
 
 MEMBER = 'member'

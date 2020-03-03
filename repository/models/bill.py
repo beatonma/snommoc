e349ea7dc5c9e65a03d24fcabff200714b5615c6
Michael Beaton <beatonma@gmail.com>
2020-03-03 15:02:17 +0000

    Bill.bill_chapter type changed PositiveIntegerField -> CharField as chapters may be denoted in Roman numerals (or perhaps other systems) Added __str__ implementations to all Bill-related models. Replaced on_delete DO_NOTHING with CASCADE

--- repository/models/bill.py
+++ repository/models/bill.py
@@ -18,6 +18,9 @@ class BillType(BaseModel):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=512)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class BillSponsor(BaseModel):
     name = models.CharField(
@@ -38,15 +41,21 @@ class BillSponsor(BaseModel):
             ['person', 'bill'],
         ]
 
+    def __str__(self):
+        return f'{self.bill}: {self.name if self.name else self.person}'
+
 
 class BillStageType(ParliamentDotUkMixin, BaseModel):
     name = models.CharField(max_length=512)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class BillStage(ParliamentDotUkMixin, BaseModel):
     bill = models.ForeignKey(
         'Bill',
-        on_delete=models.DO_NOTHING,
+        on_delete=models.CASCADE,
     )
 
     session = models.ForeignKey(
@@ -57,41 +66,52 @@ class BillStage(ParliamentDotUkMixin, BaseModel):
 
     bill_stage_type = models.ForeignKey(
         'BillStageType',
-        on_delete=models.DO_NOTHING,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
     )
+
+    def __str__(self):
+        return f'{self.bill_stage_type}: {self.bill}'
 
 
 class BillStageSitting(ParliamentDotUkMixin, BaseModel):
     bill_stage = models.ForeignKey(
         'BillStage',
-        on_delete=models.DO_NOTHING,
+        on_delete=models.CASCADE,
         related_name='sittings',
     )
-    date = models.DateField()
+    date = models.DateField(blank=True, null=True)
     formal = models.BooleanField()
     provisional = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.date}: {self.bill_stage}'
 
 
 class BillPublication(ParliamentDotUkMixin, BaseModel):
     bill = models.ForeignKey(
         'Bill',
-        on_delete=models.DO_NOTHING,
+        on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=512)
+
+    def __str__(self):
+        return f'{self.parliamentdotuk}: {self.title}'
 
 
 class Bill(ParliamentDotUkMixin, BaseModel):
     """A proposal for a new law, or a proposal to change an existing law that
     is presented for debate before Parliament."""
     title = models.CharField(max_length=512)
-    description = models.CharField(max_length=1024)
-    act_name = models.CharField(max_length=512)
+    description = models.CharField(max_length=1024, null=True, blank=True)
+    act_name = models.CharField(max_length=512, null=True, blank=True)
     label = models.CharField(max_length=512)
     homepage = models.URLField()
     date = models.DateField()
 
     ballot_number = models.PositiveIntegerField(default=0)
-    bill_chapter = models.PositiveIntegerField(default=0)
+    bill_chapter = models.CharField(max_length=64, blank=True, null=True)
 
     is_private = models.BooleanField(default=False)
     is_money_bill = models.BooleanField(default=False)
@@ -102,6 +122,7 @@ class Bill(ParliamentDotUkMixin, BaseModel):
         'BillType',
         on_delete=models.CASCADE,
         null=True,
+        blank=True,
     )
     session = models.ForeignKey(
         'ParliamentarySession',
@@ -109,3 +130,6 @@ class Bill(ParliamentDotUkMixin, BaseModel):
         null=True,
         related_name='bills',
     )
+
+    def __str__(self):
+        return f'{self.parliamentdotuk}: {self.title}'

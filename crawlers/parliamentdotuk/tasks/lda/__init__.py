4b19f5403ad940695ed1d3902dff2dd337586d7c
Michael Beaton <beatonma@gmail.com>
2020-03-05 14:09:48 +0000

    Import tasks from .bills and .divisions for celery discovery

--- crawlers/parliamentdotuk/tasks/lda/__init__.py
+++ crawlers/parliamentdotuk/tasks/lda/__init__.py
@@ -1,1 +1,7 @@
 from .update_constituencies import update_constituencies
+from .bills import update_bills
+from .divisions import (
+    update_all_divisions,
+    update_lords_divisions,
+    update_commons_divisions,
+)

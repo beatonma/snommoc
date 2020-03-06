597600d3a7695ee5b47e621f1b5497df1ef999f7
Michael Beaton <beatonma@gmail.com>
2020-03-06 15:04:34 +0000

    Added -fix to try and fix any bills that generated a BillUpdateError. Added --id to update a specific bill by parliamentdotuk id.

--- crawlers/parliamentdotuk/management/commands/bills.py
+++ crawlers/parliamentdotuk/management/commands/bills.py
@@ -4,10 +4,17 @@ Update Bills with:
 """
 
 import logging
+from typing import Optional
 
 from django.core.management import BaseCommand
 
-from crawlers.parliamentdotuk.tasks.lda.bills import update_bills
+from crawlers.parliamentdotuk.models import BillUpdateError
+from crawlers.parliamentdotuk.tasks.lda import endpoints
+from crawlers.parliamentdotuk.tasks.lda.bills import (
+    update_bills,
+    _update_bill,
+)
+from crawlers.parliamentdotuk.tasks.lda.lda_client import get_item_data
 from repository.models import (
     Bill,
     BillSponsor,
@@ -33,6 +40,16 @@ class Command(BaseCommand):
             action='store_true',
             help='Pass update_bills to Celery.',
         )
+        parser.add_argument(
+            '--id',
+            type=int,
+            help='Update a specific bill by parliamentdotuk ID',
+        )
+        parser.add_argument(
+            '-fix',
+            action='store_true',
+            help='Try to fix bills that previously generated a BillUpdatedError',
+        )
 
     def handle(self, *args, **options):
         if options['clear']:
@@ -47,9 +64,43 @@ class Command(BaseCommand):
             ]:
                 M.objects.all().delete()
 
-        elif options['async']:
-            log.info('Passing task "update_bills" to celery')
-            update_bills.delay()
+        func_kwargs = {}
+        if options['id']:
+            func = update_bill
+            func_kwargs = {
+                'parliamentdotuk': options['id'],
+            }
+        else:
+            func = update_bills
+
+        if options['async']:
+            func.delay(**func_kwargs)
 
         else:
-            update_bills()
+            func(**func_kwargs)
+
+
+def update_bill(parliamentdotuk: int) -> Optional[str]:
+    log.info(f'Updating bill #{parliamentdotuk}')
+    try:
+        data = get_item_data(endpoints.BILL.format(parliamentdotuk=parliamentdotuk))
+        log.info(data)
+        if data is None:
+            return None
+
+        return _update_bill(parliamentdotuk, data)
+    except Exception as e:
+        BillUpdateError.create(parliamentdotuk, e)
+        raise e
+
+
+def fix_errored_bills():
+    parliamentdotuk_ids = BillUpdateError.objects.values_list(
+        'parliamentdotuk',
+        flat=True
+    )
+
+    for parliamentdotuk in parliamentdotuk_ids:
+        value = update_bill(parliamentdotuk)
+        if value is not None:
+            BillUpdateError.objects.filter(parliamentdotuk=parliamentdotuk).update(handled=True)

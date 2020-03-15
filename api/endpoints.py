36141782e9f041eaa1ca4896a900c73ac0fa779a
Michael Beaton <beatonma@gmail.com>
2020-03-15 18:21:09 +0000

    Member-based endpoints now contained in /member/ Divisions: /division/commons/, /division/lords/

--- api/endpoints.py
+++ api/endpoints.py
@@ -6,24 +6,36 @@ import logging
 
 log = logging.getLogger(__name__)
 
+_HOUSE_COMMONS = 'commons'
+_HOUSE_LORDS = 'lords'
+
 # Top level
 CONSTITUENCY = 'constituency'
 PARTY = 'party'
 MEMBER = 'member'
+DIVISIONS = 'division'
 
 # Member details
-PROFILE = 'profile'
-ADDRESS = 'address'
-COMMITTEES = 'committees'
-CONSTITUENCIES = 'constituencies'
-CONTESTED_ELECTIONS = 'contested'
-DECLARED_INTERESTS = 'interests'
-ELECTIONS = 'elections'
-EXPERIENCES = 'experiences'
-MAIDEN_SPEECHES = 'speeches'
-PARTIES = 'parties'
-POSTS = 'posts'
-SUBJECTS_OF_INTEREST = 'subjects'
+PROFILE = f'{MEMBER}/profile'
+ADDRESS = f'{MEMBER}/address'
+COMMITTEES = f'{MEMBER}/committees'
+CONSTITUENCIES = f'{MEMBER}/constituencies'
+CONTESTED_ELECTIONS = f'{MEMBER}/contested'
+DECLARED_INTERESTS = f'{MEMBER}/interests'
+ELECTIONS = f'{MEMBER}/elections'
+EXPERIENCES = f'{MEMBER}/experiences'
+MAIDEN_SPEECHES = f'{MEMBER}/speeches'
+PARTIES = f'{MEMBER}/parties'
+POSTS = f'{MEMBER}/posts'
+SUBJECTS_OF_INTEREST = f'{MEMBER}/subjects'
+VOTES = f'{MEMBER}/votes'
+
+# Divisions
+DIVISION_COMMONS = f'{DIVISIONS}/{_HOUSE_COMMONS}'
+DIVISION_LORDS = f'{DIVISIONS}/{_HOUSE_LORDS}'
+
+# Bills
+BILL = 'bill'
 
 
 def endpoint_list(endpoint_name):

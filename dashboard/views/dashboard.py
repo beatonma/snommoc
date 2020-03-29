92a4c22146de1c9c9156d4639321da329b7ec92f
Michael Beaton <beatonma@gmail.com>
2020-03-29 16:32:58 +0100

    Add featured_people and featured_bills to main dashboard kwargs.

--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -1,20 +1,16 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.db.models import (
-    QuerySet,
-)
+from django.db.models import Q
 from django.shortcuts import render
+from django.utils.datetime_safe import datetime
 from django.views import View
 
 from notifications.models import TaskNotification
-from repository.models import (
-    Constituency,
-    Party,
-    Person,
-)
+
 from repository.models.constituency import UnlinkedConstituency
-from repository.models.houses import (
-    HOUSE_OF_COMMONS,
-    HOUSE_OF_LORDS,
+
+from surface.models import (
+    FeaturedPerson,
+    FeaturedBill,
 )
 
 
@@ -36,112 +32,112 @@ class BaseDashboardView(UserPassesTestMixin, View):
 
 class DashboardView(BaseDashboardView):
     def get(self, request):
+        featured_filter = Q(end__isnull=True) | Q(end__gt=datetime.today())
         return render(
             request,
             'staff-dashboard.html',
             with_unread_notifications({
-                'mp_count': Person.objects.count(),
-                'constituency_count': Constituency.objects.count(),
-                'party_count': Party.objects.count(),
+                'featured_people': FeaturedPerson.objects.filter(featured_filter),
+                'featured_bills': FeaturedBill.objects.filter(featured_filter),
             })
         )
-
-
-class MemberDashboardView(BaseDashboardView):
-    def get(self, request, *args, **kwargs):
-        if kwargs.get('requirement') == 'active':
-            members: QuerySet = Person.objects.filter(
-                active=True,
-            ).prefetch_related('party') \
-                .order_by('family_name')
-        else:
-            members = Person.objects.prefetch_related('party') \
-                .order_by('family_name')
-        return render(
-            request,
-            'dashboard-all-members.html',
-            with_unread_notifications({
-                'items': members,
-            })
-        )
-
-
-class MpDashboardView(BaseDashboardView):
-    def get(self, request, *args, **kwargs):
-        if kwargs.get('requirement') == 'active':
-            mps: QuerySet = Person.objects.filter(
-                house__name=HOUSE_OF_COMMONS,
-                active=True,
-            ).prefetch_related('party') \
-                .order_by('family_name')
-        else:
-            mps = Person.objects.filter(
-                house__name=HOUSE_OF_COMMONS,
-            ).prefetch_related('party') \
-                .order_by('family_name')
-        return render(
-            request,
-            'dashboard-all-mps.html',
-            with_unread_notifications({
-                'items': mps,
-            })
-        )
-
-
-class LordDashboardView(BaseDashboardView):
-    def get(self, request, *args, **kwargs):
-        if kwargs.get('requirement') == 'active':
-            lords: QuerySet = Person.objects.filter(
-                house__name=HOUSE_OF_LORDS,
-                active=True,
-            ).prefetch_related('party') \
-                .order_by('family_name')
-        else:
-            lords = Person.objects.filter(
-                house__name=HOUSE_OF_LORDS,
-            ).prefetch_related('party') \
-                .order_by('family_name')
-        return render(
-            request,
-            'dashboard-all-lords.html',
-            with_unread_notifications({
-                'items': lords,
-            })
-        )
-
-
-class ConstituencyDashboardView(BaseDashboardView):
-    def get(self, request, *args, **kwargs):
-        if kwargs.get('requirement') == 'active':
-            constituencies = Constituency.objects.filter(mp__active=True).order_by('name')
-        else:
-            constituencies = Constituency.objects.order_by('name')
-        return render(
-            request,
-            'dashboard-all-constituencies.html',
-            with_unread_notifications({
-                'items': constituencies,
-            })
-        )
-
-
-class PartyDashboardView(BaseDashboardView):
-    def get(self, request, *args, **kwargs):
-        if kwargs.get('requirement') == 'active':
-            mps = Person.objects.filter(active=True, house__name=HOUSE_OF_COMMONS).prefetch_related('party')
-            active_parties = [mp.party.id for mp in mps]
-
-            parties = Party.objects.filter(id__in=active_parties)
-
-            # parties = Party.objects.annotate(mp_population=Count('people')) \
-            #     .exclude(mp_population=0) \
-            #     .order_by('-mp_population')
-        else:
-            parties = Party.objects.order_by('name')
-        return render(
-            request,
-            'dashboard-all-parties.html',
-            with_unread_notifications({
-                'items': parties,
-            })
-        )
+#
+#
+# class MemberDashboardView(BaseDashboardView):
+#     def get(self, request, *args, **kwargs):
+#         if kwargs.get('requirement') == 'active':
+#             members: QuerySet = Person.objects.filter(
+#                 active=True,
+#             ).prefetch_related('party') \
+#                 .order_by('family_name')
+#         else:
+#             members = Person.objects.prefetch_related('party') \
+#                 .order_by('family_name')
+#         return render(
+#             request,
+#             'dashboard-all-members.html',
+#             with_unread_notifications({
+#                 'items': members,
+#             })
+#         )
+#
+#
+# class MpDashboardView(BaseDashboardView):
+#     def get(self, request, *args, **kwargs):
+#         if kwargs.get('requirement') == 'active':
+#             mps: QuerySet = Person.objects.filter(
+#                 house__name=HOUSE_OF_COMMONS,
+#                 active=True,
+#             ).prefetch_related('party') \
+#                 .order_by('family_name')
+#         else:
+#             mps = Person.objects.filter(
+#                 house__name=HOUSE_OF_COMMONS,
+#             ).prefetch_related('party') \
+#                 .order_by('family_name')
+#         return render(
+#             request,
+#             'dashboard-all-mps.html',
+#             with_unread_notifications({
+#                 'items': mps,
+#             })
+#         )
+#
+#
+# class LordDashboardView(BaseDashboardView):
+#     def get(self, request, *args, **kwargs):
+#         if kwargs.get('requirement') == 'active':
+#             lords: QuerySet = Person.objects.filter(
+#                 house__name=HOUSE_OF_LORDS,
+#                 active=True,
+#             ).prefetch_related('party') \
+#                 .order_by('family_name')
+#         else:
+#             lords = Person.objects.filter(
+#                 house__name=HOUSE_OF_LORDS,
+#             ).prefetch_related('party') \
+#                 .order_by('family_name')
+#         return render(
+#             request,
+#             'dashboard-all-lords.html',
+#             with_unread_notifications({
+#                 'items': lords,
+#             })
+#         )
+#
+#
+# class ConstituencyDashboardView(BaseDashboardView):
+#     def get(self, request, *args, **kwargs):
+#         if kwargs.get('requirement') == 'active':
+#             constituencies = Constituency.objects.filter(mp__active=True).order_by('name')
+#         else:
+#             constituencies = Constituency.objects.order_by('name')
+#         return render(
+#             request,
+#             'dashboard-all-constituencies.html',
+#             with_unread_notifications({
+#                 'items': constituencies,
+#             })
+#         )
+#
+#
+# class PartyDashboardView(BaseDashboardView):
+#     def get(self, request, *args, **kwargs):
+#         if kwargs.get('requirement') == 'active':
+#             mps = Person.objects.filter(active=True, house__name=HOUSE_OF_COMMONS).prefetch_related('party')
+#             active_parties = [mp.party.id for mp in mps]
+#
+#             parties = Party.objects.filter(id__in=active_parties)
+#
+#             # parties = Party.objects.annotate(mp_population=Count('people')) \
+#             #     .exclude(mp_population=0) \
+#             #     .order_by('-mp_population')
+#         else:
+#             parties = Party.objects.order_by('name')
+#         return render(
+#             request,
+#             'dashboard-all-parties.html',
+#             with_unread_notifications({
+#                 'items': parties,
+#             })
+#         )

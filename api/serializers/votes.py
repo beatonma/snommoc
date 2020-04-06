853fd34ba5472abe2126656a538756f5fc6c9abf
Michael Beaton <beatonma@gmail.com>
2020-04-06 14:40:34 +0100

    Implemented /member/votes/ endpoint to get all commons/lords votes by a member.

---
+++ api/serializers/votes.py
@@ -0,0 +1,90 @@
+"""
+
+"""
+
+import logging
+
+from rest_framework import serializers
+
+from api.serializers import (
+    DetailedModelSerializer,
+    InlineModelSerializer,
+)
+from repository.models import (
+    CommonsDivision,
+    CommonsDivisionVote,
+    LordsDivision,
+    LordsDivisionVote,
+    Person,
+)
+
+log = logging.getLogger(__name__)
+
+
+class InlineCommonsDivisionSerializer(InlineModelSerializer):
+    class Meta:
+        model = CommonsDivision
+        fields = [
+            'parliamentdotuk',
+            'title',
+            'date',
+            'passed',
+        ]
+
+
+class InlineLordsDivisionSerializer(InlineModelSerializer):
+    class Meta:
+        model = LordsDivision
+        fields = [
+            'parliamentdotuk',
+            'title',
+            'date',
+            'passed',
+        ]
+
+
+class CommonsVotesSerializer(InlineModelSerializer):
+    division = InlineCommonsDivisionSerializer()
+
+    class Meta:
+        model = CommonsDivisionVote
+        fields = [
+            'division',
+            'vote_type',
+        ]
+
+
+class LordsVotesSerializer(InlineModelSerializer):
+    division = InlineLordsDivisionSerializer()
+
+    class Meta:
+        model = CommonsDivisionVote
+        fields = [
+            'division',
+            'vote_type',
+        ]
+
+
+class MemberVotesSerializer(DetailedModelSerializer):
+    """Votes by a Person, ordered with most recent first."""
+    commons = serializers.SerializerMethodField()
+    lords = serializers.SerializerMethodField()
+
+    def get_commons(self, person):
+        return self._get(person, CommonsDivisionVote, CommonsVotesSerializer)
+
+    def get_lords(self, person):
+        return self._get(person, LordsDivisionVote, LordsVotesSerializer)
+
+    def _get(self, person, model, serializer_model):
+        qset = model.objects.filter(person=person) \
+            .prefetch_related('division') \
+            .order_by('-division__date')
+        return serializer_model(qset, many=True).data
+
+    class Meta:
+        model = Person
+        fields = [
+            'commons',
+            'lords',
+        ]
--- api/views/viewsets/procedure.py
+++ api/views/viewsets/procedure.py
@@ -6,10 +6,18 @@ import logging
 
 from django.db.models import Q
 
+from api.serializers import (
+    CommonsDivisionSerializer,
+    LordsDivisionSerializer,
+)
 from api.serializers.bills import BillSerializer
 from api.serializers.inline import InlineBillSerializer
 from api.views.viewsets import KeyRequiredViewSet
-from repository.models import Bill
+from repository.models import (
+    Bill,
+    CommonsDivision,
+    LordsDivision,
+)
 from surface.models import FeaturedBill
 
 log = logging.getLogger(__name__)
@@ -32,3 +40,17 @@ class FeaturedBillsViewSet(KeyRequiredViewSet):
             Q(end__isnull=True) | Q(end__gte=today)
         ).select_related('bill')
         return [item.bill for item in qs]
+
+
+class CommonsDivisionViewSet(KeyRequiredViewSet):
+    """Return information about a Commons division, including vote results.."""
+    queryset = CommonsDivision.objects.all()
+
+    serializer_class = CommonsDivisionSerializer
+
+
+class LordsDivisionViewSet(KeyRequiredViewSet):
+    """Return information about a Lords division, including vote results.."""
+    queryset = LordsDivision.objects.all()
+
+    serializer_class = LordsDivisionSerializer

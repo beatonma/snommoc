dd4e641f5653cac14eb94894f53d20b49b77c336
Michael Beaton <beatonma@gmail.com>
2020-06-04 16:41:53 +0100

    Created signal reciever to create a UserToken automatically for any new User that is created.

---
+++ social/signals/on_user_created.py
@@ -0,0 +1,39 @@
+"""
+
+"""
+
+import logging
+
+from django.conf import settings
+from django.db import IntegrityError
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from social.models.token import (
+    SignInServiceProvider,
+    UserToken,
+)
+
+log = logging.getLogger(__name__)
+
+
+@receiver(
+    post_save,
+    sender=settings.AUTH_USER_MODEL,
+    dispatch_uid='create_usertoken_on_user_created'
+)
+def create_usertoken_on_user_created(sender, instance, created, **kwargs):
+    if created:
+        _create_usertoken_for_user(instance)
+
+
+def _create_usertoken_for_user(instance):
+    signin_provider, _ = SignInServiceProvider.objects.get_or_create(name='snommoc.org')
+    try:
+        UserToken.objects.create(
+            provider=signin_provider,
+            provider_account_id=instance.username,
+            username=instance.username,
+        ).save()
+    except IntegrityError as e:
+        log.warning(f'Unable to create UserToken for new user: {e}')

8b6a9cce1e86acf75bd44a7558350abf5f6f40b1
Michael Beaton <beatonma@gmail.com>
2020-08-20 15:09:40 +0100

    DeletionPendingMixin:   DELETION_PENDING_PERIOD_DAYS -> DELETION_PENDING_PERIOD_HOURS   Implemented expires_at, hours_until_expired

--- social/models/mixins.py
+++ social/models/mixins.py
@@ -1,7 +1,7 @@
 """
 
 """
-
+import datetime
 import logging
 
 from django.contrib.contenttypes.fields import GenericForeignKey
@@ -35,23 +35,23 @@ class GenericTargetMixin(models.Model):
         abstract = True
 
 
-class DeletionPendingMixin:
+class DeletionPendingMixin(models.Model):
     """
     Mixin for models that may be deleted by a user.
 
     When deletion is requested, the instance should be marked
     as pending_deletion=True. The instance will no longer be available
     to public requests but will persist for a period of up to
-    DELETION_PENDING_PERIOD_DAYS days to allow review by admins for potential
+    DELETION_PENDING_PERIOD_HOURS hours to allow review by admins for potential
     abuse (e.g. posting abusive comments then deleting/remaking account), or
     to allow the user to request the instance be restored.
 
     After that period, the content will be irreversibly deleted.
     """
-    DELETION_PENDING_PERIOD_DAYS = 14
+    DELETION_PENDING_PERIOD_HOURS = 14 * 24  # 14 days
 
     pending_deletion = models.BooleanField(default=False)
-    deletion_requested = models.DateTimeField(
+    deletion_requested_at = models.DateTimeField(
         null=True,
         blank=True,
         default=None
@@ -59,17 +59,29 @@ class DeletionPendingMixin:
 
     def mark_pending_deletion(self):
         self.pending_deletion = True
-        self.deletion_requested = timezone.now()
+        self.deletion_requested_at = timezone.now()
+
+    def expires_at(self) -> datetime.datetime:
+        return self.deletion_requested_at + datetime.timedelta(
+            hours=DeletionPendingMixin.DELETION_PENDING_PERIOD_HOURS
+        )
+
+    def hours_until_expired(self):
+        now = timezone.now()
+        return (self.expires_at() - now) / datetime.timedelta(hours=1)
 
     def is_expired(self):
         now = timezone.now()
-        return (now - self.deletion_requested).days > DeletionPendingMixin.DELETION_PENDING_PERIOD_DAYS
+        return self.expires_at() < now
 
     def create_placeholder(self):
         """Create an 'empty' version of this instance to stand in place of
         the deleted content. e.g. An empty comment with the timestamps of the
         original, a 'deleted' user account..."""
         pass
+
+    class Meta:
+        abstract = True
 
 
 def get_target_kwargs(target: models.Model) -> dict:

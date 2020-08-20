106b7fe6ccea6478737fd56a44f314564d5804d1
Michael Beaton <beatonma@gmail.com>
2020-08-20 13:45:44 +0100

    UserToken now implements DeletionPendingMixin.

--- social/models/token.py
+++ social/models/token.py
@@ -8,6 +8,7 @@ import uuid
 from django.db import models
 
 from repository.models.mixins import BaseModel
+from social.models.mixins import DeletionPendingMixin
 
 log = logging.getLogger(__name__)
 
@@ -23,7 +24,7 @@ class SignInServiceProvider(BaseModel):
         return self.name
 
 
-class UserToken(BaseModel):
+class UserToken(DeletionPendingMixin, BaseModel):
     provider = models.ForeignKey(
         'SignInServiceProvider',
         on_delete=models.SET_NULL,
@@ -38,6 +39,10 @@ class UserToken(BaseModel):
     token = models.UUIDField(default=uuid.uuid4)
     username = models.CharField(max_length=16, default=_create_default_username, unique=True)
     enabled = models.BooleanField(default=True)
+
+    def mark_pending_deletion(self):
+        super().mark_pending_deletion()
+        self.enabled = False
 
     def __str__(self):
         return f'{self.provider}: {self.token}'

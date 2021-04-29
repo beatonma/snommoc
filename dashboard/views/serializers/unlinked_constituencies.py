2dff49b3d65734693277680dba27e0ac67358fd6
Michael Beaton <beatonma@gmail.com>
2021-04-29 17:30:31 +0100

    Fixed serializer after renaming model field mp -> person

--- dashboard/views/serializers/unlinked_constituencies.py
+++ dashboard/views/serializers/unlinked_constituencies.py
@@ -6,13 +6,13 @@ from repository.models import UnlinkedConstituency
 
 class UnlinkedConstituencySerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField()
-    mp = serializers.SerializerMethodField()
+    person = serializers.SerializerMethodField()
     election = serializers.SerializerMethodField()
 
     def get_url(self, obj):
         return reverse("admin:repository_unlinkedconstituency_change", args=[obj.pk])
 
-    def get_mp(self, obj):
+    def get_person(self, obj):
         return {
             "name": obj.person.name,
             "url": reverse("member-detail", args=[obj.person.pk]),

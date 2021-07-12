5611aa5c2a24929a7ab7ae9a7c966518dffdfd39
Michael Beaton <beatonma@gmail.com>
2021-07-12 16:53:10 +0100

    Added wikipedia_path to profile weblinks list when available.

--- api/serializers/address.py
+++ api/serializers/address.py
@@ -4,6 +4,8 @@
 
 import logging
 
+from rest_framework.fields import SerializerMethodField
+
 from api.serializers import DetailedModelSerializer
 from repository.models import (
     PhysicalAddress,
@@ -18,12 +20,12 @@ class PhysicalAddressSerializer(DetailedModelSerializer):
     class Meta:
         model = PhysicalAddress
         fields = [
-            'description',
-            'address',
-            'postcode',
-            'phone',
-            'fax',
-            'email',
+            "description",
+            "address",
+            "postcode",
+            "phone",
+            "fax",
+            "email",
         ]
 
 
@@ -31,19 +33,36 @@ class WebAddressSerializer(DetailedModelSerializer):
     class Meta:
         model = WebAddress
         fields = [
-            'description',
-            'url',
+            "description",
+            "url",
         ]
 
 
 class AddressSerializer(DetailedModelSerializer):
-    physical = PhysicalAddressSerializer(many=True, source='physicaladdress_set')
-    web = WebAddressSerializer(many=True, source='webaddress_set')
+    physical = PhysicalAddressSerializer(many=True, source="physicaladdress_set")
+    web = SerializerMethodField()
 
     class Meta:
         model = Person
 
         fields = [
-            'physical',
-            'web',
+            "physical",
+            "web",
         ]
+
+    def get_web(self, obj):
+        """
+        Related WebAddress instances with partial Wikipedia path, if available.
+        Full url needs to be constructed by client (where they can apply localisation as required).
+        """
+        wiki = obj.wikipedia
+        links = obj.webaddress_set
+        if wiki:
+            links = list(links.values()) + [
+                {
+                    "description": "wikipedia_path",
+                    "url": wiki,
+                }
+            ]
+
+        return WebAddressSerializer(links, many=True).data

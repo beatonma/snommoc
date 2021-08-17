e534215c4efa942dd9f31b271c182e5960cc6bf5
Michael Beaton <beatonma@gmail.com>
2021-08-17 13:39:44 +0100

    Enabled caching for puk memberportraits.

--- crawlers/parliamentdotuk/tasks/membersdataplatform/member_portrait.py
+++ crawlers/parliamentdotuk/tasks/membersdataplatform/member_portrait.py
@@ -1,8 +1,6 @@
 import logging
 import re
-from typing import (
-    Optional,
-)
+from typing import Optional
 
 from django.db.models import QuerySet
 
@@ -14,7 +12,7 @@ from notifications.models.task_notification import task_notification
 from repository.models import Person
 from repository.models.portrait import MemberPortrait
 from repository.resolution import get_active_members
-from crawlers.network import get_json
+from crawlers.network import get_json, json_cache
 from crawlers.wikipedia import wikipedia_client
 
 log = logging.getLogger(__name__)
@@ -29,51 +27,53 @@ CROP_WIDE = "ThreeTwo"
 CROP_TALL = "ThreeFour"
 
 
-def _update_official_portraits(csv_data: str):
-    lines = csv_data.splitlines()
-    for line in lines[1:]:
-        parliamentdotuk, _name, _, _, year_taken, full, wide, tall, square = line.split(
-            ","
+def _update_official_member_portrait(member: Person, portrait_url: str):
+    def _url_with_croptype(base_url: str, croptype: str):
+        return f"{base_url}?cropType={croptype}"
+
+    base_url = MEMBER_PORTRAIT_URL_PATTERN.match(portrait_url).group(1)
+
+    try:
+        MemberPortrait.objects.update_or_create(
+            person_id=member.parliamentdotuk,
+            defaults={
+                "fullsize_url": _url_with_croptype(base_url, CROP_FULLSIZE),
+                "square_url": _url_with_croptype(base_url, CROP_SQUARE),
+                "wide_url": _url_with_croptype(base_url, CROP_WIDE),
+                "tall_url": _url_with_croptype(base_url, CROP_TALL),
+            },
         )
+    except:
+        log.warning(f"Failed to update portrait data for member {member}")
 
+
+@json_cache(name="memberportraits")
+def _update_official_member_portraits(members: QuerySet[Person], **kwargs):
+    """Try to get portrait data from the official parliament.uk API for each given member."""
+
+    def _get_portrait_url(member_id: int) -> Optional[str]:
         try:
-            Person.objects.get(parliamentdotuk=parliamentdotuk)
-            MemberPortrait.objects.update_or_create(
-                person_id=int(parliamentdotuk),
-                defaults={
-                    "year_taken": int(year_taken),
-                    "fullsize_url": full,
-                    "wide_url": wide,
-                    "tall_url": tall,
-                    "square_url": square,
-                },
+            data = get_json(
+                endpoints.MEMBER_PORTRAIT_URL.format(id=member_id), **kwargs
             )
+            return data.get("value")
         except Exception as e:
-            log.warning(f"Unknown person {_name} id={parliamentdotuk}: {e}")
+            log.warning(f"Could not get portrait url [{member_id}]: {e}")
+
+    for m in members:
+        url = _get_portrait_url(m.parliamentdotuk)
+        if url is not None:
+            _update_official_member_portrait(m, url)
+
+        rate_limit()
 
 
-def _get_wikipedia_portraits(members: QuerySet[Person]):
+def _update_wikipedia_member_portraits(members: QuerySet[Person]):
     """
-    Sample response:
-        {
-          "batchcomplete": "",
-          "query": {
-            "pages": {
-              "414450": {
-                "pageid": 414450,
-                "ns": 0,
-                "title": "John Baron (politician)",
-                "original": {
-                  "source": "https://upload.wikimedia.org/wikipedia/commons/e/ee/Official_portrait_of_Mr_John_Baron_MP_crop_2.jpg",
-                  "width": 1500,
-                  "height": 2000
-                }
-              }
-            }
-          }
-        }
+    Sample url:
+    https://en.wikipedia.org/w/api.php?action=query&titles=Dawn_Butler&prop=pageimages&piprop=original&format=json&pilicense=free
     """
-    print(f"Looking for portraits for {members.count()} members...")
+    log.debug(f"Looking for portraits for {members.count()} members...")
     wiki_pages = [m.wikipedia for m in members if m.wikipedia]
 
     images_map = {}
@@ -123,53 +123,16 @@ def _get_wikipedia_portraits(members: QuerySet[Person]):
         )
 
 
-def _get_portrait_url(member_id: int, **kwargs) -> Optional[str]:
-    try:
-        data = get_json(endpoints.MEMBER_PORTRAIT_URL.format(id=member_id), **kwargs)
-        return data.get("value")
-    except Exception as e:
-        log.warning(f"Could not get portrait url [{member_id}]: {e}")
-        return None
-
-
-def _update_member_portrait(member: Person, portrait_url: str):
-    def _url_with_croptype(base_url: str, croptype: str):
-        return f"{base_url}?cropType={croptype}"
-
-    base_url = MEMBER_PORTRAIT_URL_PATTERN.match(portrait_url).group(1)
-
-    try:
-        MemberPortrait.objects.update_or_create(
-            person_id=member.parliamentdotuk,
-            defaults={
-                "fullsize_url": _url_with_croptype(base_url, CROP_FULLSIZE),
-                "square_url": _url_with_croptype(base_url, CROP_SQUARE),
-                "wide_url": _url_with_croptype(base_url, CROP_WIDE),
-                "tall_url": _url_with_croptype(base_url, CROP_TALL),
-            },
-        )
-    except:
-        log.warning(f"Failed to update portrait data for member {member}")
-
-
 @shared_task
 @task_notification(label="UpdateMemberPortraits")
 def update_member_portraits(**kwargs):
     active_members = get_active_members()
 
-    for m in active_members:
-        url = _get_portrait_url(m.parliamentdotuk, **kwargs)
-        if url is not None:
-            _update_member_portrait(m, url)
-
-        rate_limit()
+    _update_official_member_portraits(active_members, **kwargs)
 
     members_without_portraits = get_active_members(memberportrait__isnull=True)
-    log.info(
-        f"There are {members_without_portraits.count()} active members with no official portrait."
-    )
 
-    _get_wikipedia_portraits(members_without_portraits)
+    _update_wikipedia_member_portraits(members_without_portraits)
 
 
 @shared_task
@@ -182,11 +145,11 @@ def update_missing_member_portraits_wikipedia(**kwargs):
     """
     members = get_active_members(wikipedia__isnull=False, memberportrait__isnull=True)
 
-    _get_wikipedia_portraits(members)
+    _update_wikipedia_member_portraits(members)
 
 
 @shared_task
 @task_notification(label="UpdateWikipediaPortraits")
 def update_member_portraits_wikipedia(**kwargs):
     members = get_active_members(wikipedia__isnull=False)
-    _get_wikipedia_portraits(members)
+    _update_wikipedia_member_portraits(members)

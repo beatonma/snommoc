bc15ec785a37439defbca8fb21ece6914a43ba9d
Michael Beaton <beatonma@gmail.com>
2022-02-22 17:08:48 +0000

    Removed deprecated URLs for bill data.

--- crawlers/parliamentdotuk/tasks/lda/endpoints.py
+++ crawlers/parliamentdotuk/tasks/lda/endpoints.py
@@ -1,9 +1,5 @@
 CONSTITUENCIES_BASE_URL = "https://lda.data.parliament.uk/constituencies.json"
 
-# e.g. https://lda.data.parliament.uk/bills/754405.json
-BILLS = "https://lda.data.parliament.uk/bills.json"
-BILL = "https://lda.data.parliament.uk/bills/{parliamentdotuk}.json"
-
 # E.g. https://lda.data.parliament.uk/commonsdivisions/id/1171292.json
 COMMONS_DIVISIONS = "https://lda.data.parliament.uk/commonsdivisions.json"
 COMMONS_DIVISION = (
@@ -27,10 +23,6 @@ CALENDAR = "https://services.parliament.uk/calendar/all.rss"
 PARAM_PAGE_SIZE = "_pageSize"
 PARAM_PAGE = "_page"
 MAX_PAGE_SIZE = 500
-
-
-def url_for_bill(parliamentdotuk: int) -> str:
-    return BILL.format(parliamentdotuk=parliamentdotuk)
 
 
 def url_for_commons_division(parliamentdotuk: int) -> str:

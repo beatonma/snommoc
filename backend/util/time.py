3876eaded94bac7f2168d7d21544f95b00c7f3c1
Michael Beaton <beatonma@gmail.com>
2024-10-18 13:47:12 +0100

    Minor modernising refactor

--- backend/util/time.py
+++ backend/util/time.py
@@ -1,34 +1,36 @@
-import datetime
-from typing import Optional
+from datetime import date, datetime
+from typing import Callable
 
 from django.utils import timezone
 
+type _DefaultDate = Callable[[], date] | date
 
-def get_now() -> datetime.datetime:
+
+def get_now() -> datetime:
     return timezone.now()
 
 
-def get_today() -> datetime.date:
+def get_today() -> date:
     return get_now().date()
 
 
-def coerce_timezone(dt) -> datetime.datetime:
+def coerce_timezone(dt) -> datetime:
     """
     Convert a date or naive datetime to an aware datetime.
     """
-    if isinstance(dt, datetime.datetime):
+    if isinstance(dt, datetime):
         if timezone.is_aware(dt):
             return dt
         else:
             return timezone.make_aware(dt)
 
-    elif isinstance(dt, datetime.date):
-        return timezone.make_aware(datetime.datetime(dt.year, dt.month, dt.day))
+    elif isinstance(dt, date):
+        return timezone.make_aware(datetime(dt.year, dt.month, dt.day))
 
 
 def years_between(
-    date_from: Optional[datetime.date],
-    date_to: Optional[datetime.date],
+    date_from: date | None,
+    date_to: date | None,
 ) -> int:
     """Order is important! We will return zero if date_from is after date_to."""
     if not date_from or not date_to:
@@ -36,55 +38,53 @@ def years_between(
 
     difference = date_to.year - date_from.year
     if date_to.month < date_from.month:
-        difference = difference - 1
+        difference -= 1
     elif date_to.month == date_from.month and date_to.day < date_from.day:
-        difference = difference - 1
+        difference -= 1
 
     return max(difference, 0)
 
 
-def years_since(date: Optional[datetime.date], now=get_today) -> int:
+def years_since(dt: date | None, now: _DefaultDate = get_today) -> int:
     if callable(now):
         now = now()
 
-    return years_between(date, now)
+    return years_between(dt, now)
 
 
-def is_anniversary(date: Optional[datetime.date], now=get_today) -> bool:
+def is_anniversary(dt: date | None, now: _DefaultDate = get_today) -> bool:
     if callable(now):
         now = now()
 
-    if not date:
+    if not dt:
         return False
 
-    return now.month == date.month and now.day == date.day
+    return now.month == dt.month and now.day == dt.day
 
 
-def year_only_date(year: int) -> datetime.date:
+def year_only_date(year: int) -> date:
     """Some dates are only available as a year but we still want to treat them
-    as datetime.date objects. Validation for datetime.date does not allow dates
+    as date objects. Validation for date does not allow dates
     that don't fit on the calendar (we can't use the 0th or 32nd day, the 13th
     month, etc, etc) so we have to choose some date to represent a non-specific
     day of the year. We have chosen xmas day as an 'invalid' date as it seems
     perhaps the most reliably unlikely day for anything to happen in business,
     parliamentary or otherwise.
     """
-    return datetime.date(year=year, month=12, day=25)
+    return date(year=year, month=12, day=25)
 
 
-def in_range(
-    date: datetime.date, start: Optional[datetime.date], end: Optional[datetime.date]
-) -> bool:
+def in_range(dt: date, start: date | None, end: date | None) -> bool:
     if start is None:
-        return end is None or end > date
+        return end is None or end > dt
     if end is None:
-        return start is None or start <= date
+        return start is None or start <= dt
 
-    return start <= date < end
+    return start <= dt < end
 
 
 def is_current(
-    start: Optional[datetime.date], end: Optional[datetime.date], now=get_today
+    start: date | None, end: date | None, now: _DefaultDate = get_today
 ) -> bool:
     if callable(now):
         now = now()

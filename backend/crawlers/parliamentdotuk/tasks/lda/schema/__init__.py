68f1a257924552644df25f59c872c831e2193812
Michael Beaton <beatonma@gmail.com>
2024-10-29 10:17:37 +0000

    Refactor with `field` convenience function.

--- backend/crawlers/parliamentdotuk/tasks/lda/schema/__init__.py
+++ backend/crawlers/parliamentdotuk/tasks/lda/schema/__init__.py
@@ -1,5 +1,6 @@
+from crawlers.parliamentdotuk.tasks.types import field
 from pydantic import BaseModel as Schema
-from pydantic import Field, model_validator
+from pydantic import model_validator
 
 from .constituency import Constituency
 from .division import CommonsDivision, CommonsDivisionItem, Vote, VoteType
@@ -7,15 +8,13 @@ from .election_results import ElectionCandidate, ElectionResult, ElectionResultD
 
 
 class Page[T: Schema](Schema):
-    items: list[T] = Field(validation_alias="items")
-    items_per_page: int = Field(validation_alias="itemsPerPage")
+    items: list[T] = field("items")
+    items_per_page: int = field("itemsPerPage")
     page: int
-    start_index: int = Field(
-        validation_alias="startIndex"
-    )  # index of first item on the page
-    total_results: int = Field(validation_alias="totalResults")
-    prev_page_url: str | None = Field(default=None, validation_alias="prev")
-    next_page_url: str | None = Field(default=None, validation_alias="next")
+    start_index: int = field("startIndex")  # index of first item on the page
+    total_results: int = field("totalResults")
+    prev_page_url: str | None = field("prev", default=None)
+    next_page_url: str | None = field("next", default=None)
 
     @model_validator(mode="before")
     @classmethod
@@ -29,9 +28,4 @@ class Item[T: Schema](Schema):
         https://lda.data.parliament.uk/commonsdivisions/id/1720066.json
     """
 
-    data: T = Field(validation_alias="primaryTopic")
-
-    @model_validator(mode="before")
-    @classmethod
-    def unwrap(cls, obj):
-        return obj["result"]
+    data: T = field("result.primaryTopic")

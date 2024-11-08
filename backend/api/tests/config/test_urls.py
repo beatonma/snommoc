4fa88e30d55f0bab0a876afea9089d010b068354
Michael Beaton <beatonma@gmail.com>
2024-11-08 16:45:19 +0000

    Removed unused @api_key_required decorator

--- backend/api/tests/config/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from api.tests.views import ExampleView
-
-from api.urls import urlpatterns
-
-urlpatterns += [
-    path('decorator', ExampleView.as_view(), name='decorator_test_view'),
-]

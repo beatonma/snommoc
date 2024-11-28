cc64b305e197b10c151050c1230661c3be5e200b
Michael Beaton <beatonma@gmail.com>
2024-11-28 17:17:05 +0000

    Removed obsolete signal
    `ConstituencyCandidate` no longer has `party_name` field after update to openapi data source.

--- backend/repository/apps.py
+++ backend/repository/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class RepositoryConfig(AppConfig):
     name = "repository"
-
-    def ready(self):
-        from repository.signals import (  # noqa
-            update_constituencycandidate_party_when_partyaka_canonical_changed,
-        )
--- backend/repository/signals/__init__.py
+++
@@ -1,3 +0,0 @@
-from .on_partyalsoknownas_changed import (
-    update_constituencycandidate_party_when_partyaka_canonical_changed,
-)

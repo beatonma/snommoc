469be7d97f30f926a226ab42bc027e30a5db5f98
Michael Beaton <beatonma@gmail.com>
2024-12-16 10:44:32 +0000

    Fix: ensure loading current preference only runs on client.

--- frontend/src/components/remote-content.tsx
+++ frontend/src/components/remote-content.tsx
@@ -1,20 +1,10 @@
+"use client";
+
 import { ReactNode, useEffect, useState } from "react";
 import { TintedButton } from "@/components/button";
+import Loading from "@/components/loading";
 
 type RemoteContentProvider = "openstreetmap.org";
-
-const storageKeyOf = (provider: RemoteContentProvider) =>
-  `remotecontentprovider__${provider}`;
-const allowContentProvider = (provider: RemoteContentProvider): boolean => {
-  return localStorage.getItem(storageKeyOf(provider))?.toLowerCase() === "true";
-};
-
-const setAllowContentProvider = (
-  provider: RemoteContentProvider,
-  allow: boolean,
-) => {
-  localStorage.setItem(storageKeyOf(provider), `${allow}`);
-};
 
 interface RemoteContentProps {
   provider: RemoteContentProvider;
@@ -22,21 +12,35 @@ interface RemoteContentProps {
 }
 export const RemoteContent = (props: RemoteContentProps) => {
   const { provider, content } = props;
-  const [isAllowed, setIsAllowed] = useState<boolean>(
-    allowContentProvider(provider),
-  );
+  const [isAllowed, setIsAllowed] = useState<boolean>();
 
   useEffect(() => {
-    setAllowContentProvider(provider, isAllowed);
+    if (isAllowed === undefined) {
+      setIsAllowed(getSavedPreference(provider));
+      return;
+    }
+
+    setSavedPreference(provider, isAllowed);
   }, [provider, isAllowed]);
 
-  if (isAllowed) {
-    return <>{content()}</>;
-  } else {
-    return (
-      <TintedButton onClick={() => setIsAllowed(true)}>
-        {`Click to allow content from ${provider}`}
-      </TintedButton>
-    );
-  }
+  if (isAllowed === undefined) return <Loading />;
+  if (isAllowed) return <>{content()}</>;
+
+  return (
+    <TintedButton onClick={() => setIsAllowed(true)}>
+      {`Click to allow content from ${provider}`}
+    </TintedButton>
+  );
+};
+
+const storageKeyOf = (provider: RemoteContentProvider) =>
+  `remotecontentprovider__${provider}`;
+const getSavedPreference = (provider: RemoteContentProvider): boolean =>
+  window.localStorage.getItem(storageKeyOf(provider))?.toLowerCase() === "true";
+
+const setSavedPreference = (
+  provider: RemoteContentProvider,
+  allow: boolean,
+) => {
+  window.localStorage.setItem(storageKeyOf(provider), `${allow}`);
 };
